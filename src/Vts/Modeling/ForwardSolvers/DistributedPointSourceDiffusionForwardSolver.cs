983bc0a2e192195a3f6ecd276c7058d3a2c195f3
itsScience <none@none>
2010-07-06 16:25:34 -0700

    addition of xml comments

--- src/Vts/Modeling/ForwardSolvers/DistributedPointSourceDiffusionForwardSolver.cs
+++ src/Vts/Modeling/ForwardSolvers/DistributedPointSourceDiffusionForwardSolver.cs
@@ -18,7 +18,15 @@ namespace Vts.Modeling.ForwardSolvers
         }
 
         #region SteadyState
-
+        /// <summary>
+        /// Evaluation of the radially resolved stationary reflectance for the distribution of point sources in
+        /// the source-image configuration.
+        /// </summary>
+        /// <param name="dp">DiffusionParameters object</param>
+        /// <param name="rho">radial position</param>
+        /// <param name="fr1">Fresnel Reflection Moment 1</param>
+        /// <param name="fr2">Fresnel Reflection Moment 2</param>
+        /// <returns>reflectance</returns>
         public override double StationaryReflectance(
             DiffusionParameters dp, double rho, double fr1, double fr2)
         {
@@ -28,6 +36,14 @@ namespace Vts.Modeling.ForwardSolvers
                 fr1, fr2);
         }
 
+        /// <summary>
+        /// Evaluation of the radially resolved stationary fluence for the distribution of point
+        /// sources under the source-image configuration.
+        /// </summary>
+        /// <param name="rho">radial position</param>
+        /// <param name="z">depth position</param>
+        /// <param name="dp">DiffusionParameters object</param>
+        /// <returns>fluence</returns>
         public override double StationaryFluence(double rho, double z, DiffusionParameters dp)
         {
             var dpLocal = DiffusionParameters.Copy(dp);
@@ -57,7 +73,16 @@ namespace Vts.Modeling.ForwardSolvers
         #endregion SteadyState
 
         #region Temporal Solutions
-
+        /// <summary>
+        /// Evaulation of the temporally resolved radial reflectance using the distribution of
+        /// source-image point sources.
+        /// </summary>
+        /// <param name="dp">DiffusionParamters object</param>
+        /// <param name="rho">radial location</param>
+        /// <param name="t">time</param>
+        /// <param name="fr1">Fresnel Moment 1</param>
+        /// <param name="fr2">Fresnel Moment 2</param>
+        /// <returns>reflectance</returns>
         public override double TemporalReflectance(DiffusionParameters dp, double rho, double t, double fr1, double fr2)
         {
             return GetBackwardHemisphereIntegralDiffuseReflectance(
@@ -66,6 +91,15 @@ namespace Vts.Modeling.ForwardSolvers
                 fr1, fr2);
         }
 
+        /// <summary>
+        /// Evaluation of the temporally and radially resolved fluence rate using the distribution of
+        /// source-image point sources.
+        /// </summary>
+        /// <param name="dp">DiffusionParameters object</param>
+        /// <param name="rho">radial location</param>
+        /// <param name="z">depth location</param>
+        /// <param name="t">time</param>
+        /// <returns>fluence rate</returns>
         public override double TemporalFluence(
             DiffusionParameters dp, double rho, double z, double t)
         {
@@ -101,7 +135,16 @@ namespace Vts.Modeling.ForwardSolvers
 
         #region Temporal Frequency Solutions
 
-
+        /// <summary>
+        /// Evaluates the temporal frequency radially resolved reflectance using the distribution of
+        /// the source-image point source configuration.
+        /// </summary>
+        /// <param name="dp">DiffusionParameters object</param>
+        /// <param name="rho">radial location</param>
+        /// <param name="k">wavevector</param>
+        /// <param name="fr1">Fresnel Moment 1</param>
+        /// <param name="fr2">Fresnel Moment 2</param>
+        /// <returns></returns>
         public override Complex TemporalFrequencyReflectance(DiffusionParameters dp,
             double rho, Complex k, double fr1, double fr2)
         {
@@ -117,16 +160,26 @@ namespace Vts.Modeling.ForwardSolvers
                 };
 
             return
-                CalculatorToolbox.EvaluateDistributedExponentialLineSourceIntegral(
+                (CalculatorToolbox.EvaluateDistributedExponentialLineSourceIntegral(
                         zp => kernelFunc(dpLocalReal, rho, k, zp).Real,
-                        dp.mutTilde) * dp.musTilde
+                        dp.mutTilde)
                 + Complex.ImaginaryOne *
                 CalculatorToolbox.EvaluateDistributedExponentialLineSourceIntegral(
                         zp => kernelFunc(dpLocalImag, rho, k, zp).Imaginary,
-                        dp.mutTilde) * dp.musTilde;
+                        dp.mutTilde)
+                        ) * dp.musTilde;
 
         }
 
+        /// <summary>
+        /// Evaluate the temporal frequency - radially resolved fluence with the distribution
+        /// of point sources in the source-image configuration
+        /// </summary>
+        /// <param name="dp">DiffusionParameters object</param>
+        /// <param name="rho">radial location</param>
+        /// <param name="z">depth location</param>
+        /// <param name="k">wavevector</param>
+        /// <returns>fluence rate</returns>
         public override Complex TemporalFrequencyFluence(DiffusionParameters dp, double rho, double z, Complex k)
         {
             var dpLocalReal = DiffusionParameters.Copy(dp);
--- src/Vts/Modeling/ForwardSolvers/PointSourceDiffusionForwardSolver.cs
+++ src/Vts/Modeling/ForwardSolvers/PointSourceDiffusionForwardSolver.cs
@@ -8,6 +8,14 @@ namespace Vts.Modeling.ForwardSolvers
         public PointSourceDiffusionForwardSolver()
             : base(SourceConfiguration.Point, 0.0) { }
 
+        /// <summary>
+        /// Evaluate the stationary radially resolved reflectance with the point source-image configuration
+        /// </summary>
+        /// <param name="dp">DiffusionParameters object</param>
+        /// <param name="rho">radial location</param>
+        /// <param name="fr1">Fresnel Moment 1</param>
+        /// <param name="fr2">Fresnel Moment 2</param>
+        /// <returns>reflectance</returns>
         public override double StationaryReflectance(DiffusionParameters dp, double rho, double fr1, double fr2)
         {
             return GetBackwardHemisphereIntegralDiffuseReflectance(StationaryFluence(rho, 0.0, dp),
@@ -15,6 +23,14 @@ namespace Vts.Modeling.ForwardSolvers
                 fr1, fr2);
         }
 
+        /// <summary>
+        /// Evaluate the stationary radially resolved fluence with the point source-image
+        /// configuration
+        /// </summary>
+        /// <param name="rho">radial location</param>
+        /// <param name="z">depth location</param>
+        /// <param name="dp">DiffusionParameters object</param>
+        /// <returns>fluence</returns>
         public override double StationaryFluence(double rho, double z, DiffusionParameters dp)
         {
             return DiffusionGreensFunctions.StationaryPointSourceImageGreensFunction(dp,
@@ -22,16 +38,34 @@ namespace Vts.Modeling.ForwardSolvers
                 CalculatorToolbox.GetRadius(rho, z + dp.zp + 2 * dp.zb));
         }
 
+        /// <summary>
+        /// Evaluate the stationary radially resolved z-flux with the point source-image
+        /// configuration
+        /// </summary>
+        /// <param name="rho">radial location</param>
+        /// <param name="z">depth location</param>
+        /// <param name="dp">DiffusionParamters object</param>
+        /// <returns></returns>
         public double StationaryFlux(double rho, double z, DiffusionParameters dp)
         {
             var zSource = z - dp.zp;
             var zImage = z + dp.zp + 2 * dp.zb;
             return
                 DiffusionGreensFunctions.StationaryPointSourceImageGreensFunctionZFlux(dp,
-                    CalculatorToolbox.GetRadius(rho, z - dp.zp), zSource,
-                    CalculatorToolbox.GetRadius(rho, z + dp.zp + 2 * dp.zb), zImage);
+                    CalculatorToolbox.GetRadius(rho, zSource), zSource,
+                    CalculatorToolbox.GetRadius(rho, zImage), zImage);
         }
 
+        /// <summary>
+        /// Evaluate the temporally-radially resolved reflectance with the point source-image
+        /// configuration
+        /// </summary>
+        /// <param name="dp">DiffusionParameters object</param>
+        /// <param name="rho">radial location</param>
+        /// <param name="t">time</param>
+        /// <param name="fr1">Fresnel Moment 1</param>
+        /// <param name="fr2">Fresnel Moment 2</param>
+        /// <returns>reflectance</returns>
         public override double TemporalReflectance(
             DiffusionParameters dp, double rho, double t, double fr1, double fr2)
         {
@@ -41,6 +75,14 @@ namespace Vts.Modeling.ForwardSolvers
                     fr1, fr2);
         }
 
+        /// <summary>
+        /// Evaluate the temporally-radially resolved fluence using the point source-image configuration
+        /// </summary>
+        /// <param name="dp">DiffusionParameters object</param>
+        /// <param name="rho">radial location</param>
+        /// <param name="z">depth location</param>
+        /// <param name="t">time</param>
+        /// <returns>fluence</returns>
         public override double TemporalFluence(
             DiffusionParameters dp, double rho, double z, double t)
         {
@@ -50,6 +92,14 @@ namespace Vts.Modeling.ForwardSolvers
                     t);
         }
 
+        /// <summary>
+        /// Evaluate the temporally-radially resolved z-flux using the point source-image configuration
+        /// </summary>
+        /// <param name="dp">DiffusionParameters object</param>
+        /// <param name="rho">radial location</param>
+        /// <param name="z">depth location</param>
+        /// <param name="t">time</param>
+        /// <returns></returns>
         public double TemporalFlux(
             DiffusionParameters dp, double rho, double z, double t)
         {
@@ -61,6 +111,7 @@ namespace Vts.Modeling.ForwardSolvers
                 CalculatorToolbox.GetRadius(rho, zImage), zImage, 
                 t);
         }
+
 
         public override Complex TemporalFrequencyReflectance(
             DiffusionParameters dp, double rho, Complex k, double fr1, double fr2)
