39cb223d3bb74e56bee257e8f9be131617493f1e
Michele Martinelli <michelemartinelli32@gmail.com>
2010-07-07 17:10:00 -0700

    Modified methods RofRhoAndFt and RofFxAndFt Now it is possible to use analytical FT or DFT.

--- src/Vts/Modeling/ForwardSolvers/NurbsForwardSolver.cs
+++ src/Vts/Modeling/ForwardSolvers/NurbsForwardSolver.cs
@@ -24,7 +24,7 @@ namespace Vts.Modeling.ForwardSolvers
         private INurbs _rdGenerator;
         private INurbs _sfdGenerator;
 
-        public static readonly double _v =  GlobalConstants.C / 1.4;
+        public static readonly double v =  GlobalConstants.C / 1.4;
         private static readonly OpticalProperties _opReference =
                                                  new OpticalProperties(0.0, 1, 0.8, 1.4);
 
@@ -111,7 +111,7 @@ namespace Vts.Modeling.ForwardSolvers
                 foreach (var rho in rhos)
                 {
                     rho_ref = rho * op.Musp / _opReference.Musp;
-                    double exponentialTerm = op.Mua * _v * _opReference.Musp / op.Musp;
+                    double exponentialTerm = op.Mua * v * _opReference.Musp / op.Musp;
 
                     if (rho_ref <= _rdGenerator.SpaceValues.MaxValue)
                     {
@@ -196,7 +196,7 @@ namespace Vts.Modeling.ForwardSolvers
 
                         scaledValue = CheckIfValidOutput(scaledValue);
                         
-                        yield return scalingFactor * scaledValue * Math.Exp(-op.Mua * _v * t);
+                        yield return scalingFactor * scaledValue * Math.Exp(-op.Mua * v * t);
                     }
                 }
             }
@@ -218,8 +218,9 @@ namespace Vts.Modeling.ForwardSolvers
         /// <summary>
         ///  Evaluates the temporal frequency and space resolved reflectance at a source 
         ///  detector separation rho for a modulation frequency ft,for the specified 
-        ///  optical properties. It calculates the analitycal Fourier transform of the NURBS
-        ///  curve R(t) at the required source detector separation. 
+        ///  optical properties. It calculates the Fourier transform of the NURBS
+        ///  curve R(t) at the required source detector separation.
+        ///  The used FT is analitycal or discrete according to the boolean value 'analyticIntegration'.
         /// </summary>
         /// <param name="ops">optical properties</param>
         /// <param name="rhos">source detector separation</param>
@@ -227,6 +228,7 @@ namespace Vts.Modeling.ForwardSolvers
         /// <returns>reflectance intensity</returns>
         public override IEnumerable<Complex> RofRhoAndFt(IEnumerable<OpticalProperties> ops, IEnumerable<double> rhos, IEnumerable<double> fts)
         {
+            bool analyticIntegration = false;
             double scalingFactor;
             double rho_ref;
             Complex transformedValue;
@@ -234,45 +236,59 @@ namespace Vts.Modeling.ForwardSolvers
             foreach (var op in ops)
             {
                 scalingFactor = GetScalingFactor(op, 2);
-
-                foreach (var rho in rhos)
+                
+                if (analyticIntegration)
                 {
-                    rho_ref = rho * op.Musp / _opReference.Musp;
-                    double exponentialTerm = op.Mua * _v * _opReference.Musp / op.Musp;
-
-                    if (rho_ref <= _rdGenerator.SpaceValues.MaxValue)
+                    foreach (var rho in rhos)
                     {
-                        foreach (var ft in fts)//lui va scalato?..direi di no perche si tratta di trasformare a posizione scalata.
+                        rho_ref = rho * op.Musp / _opReference.Musp;
+                        double exponentialTerm = op.Mua * v * _opReference.Musp / op.Musp;
+
+                        if (rho_ref <= _rdGenerator.SpaceValues.MaxValue)
                         {
-                            transformedValue = _rdGenerator.EvaluateNurbsCurveFourierTransform(rho_ref, exponentialTerm, ft * _opReference.Musp / op.Musp);
-                            yield return transformedValue * scalingFactor;
+                            foreach (var ft in fts)
+                            {
+                                transformedValue = _rdGenerator.EvaluateNurbsCurveFourierTransform(rho_ref, exponentialTerm, ft * _opReference.Musp / op.Musp);
+                                yield return transformedValue * scalingFactor;
+                            }
+                        }
+                        else
+                        {
+                            foreach (var ft in fts)
+                            {
+                                yield return new Complex(0.0, 0.0);
+                            }
                         }
                     }
-                    else
+                }
+                else
+                {
+                    var time = _rdGenerator.TimeKnotSpanPolynomialCoefficients.Select(span => span.GetKnotSpanMidTime());
+                    var deltaT = _rdGenerator.TimeKnotSpanPolynomialCoefficients.Select(span => span.GetKnotSpanDeltaT());
+
+                    foreach (var rho in rhos)
                     {
-                        foreach (var ft in fts)
+                        rho_ref = rho * op.Musp / _opReference.Musp;
+                        if (rho_ref <= _rdGenerator.SpaceValues.MaxValue)
                         {
-                            yield return new Complex(0.0,0.0); 
+                            var RofT = RofRhoAndT(op.AsEnumerable(), rho_ref.AsEnumerable(), time);
+
+                            foreach (var ft in fts)
+                            {
+                                transformedValue = LinearDiscreteFourierTransform.GetFourierTransform(time.ToArray(), RofT.ToArray(), deltaT.ToArray(), ft * _opReference.Musp / op.Musp);
+                                yield return transformedValue * scalingFactor;
+                            }
+                        }
+                        else
+                        {
+                            foreach (var ft in fts)
+                            {
+                                yield return new Complex(0.0, 0.0);
+                            }
                         }
                     }
                 }
             }
-            //foreach (var op in ops)
-            //{
-            //    double maxT = _rdGenerator.TimeValues.MaxValue * _opReference.Musp / op.Musp;
-            //    double maxFt = fts.Last();
-            //    double nPoints = maxT * 2.0 * maxFt;
-            //    var time =  new DoubleRange(0.0, maxT, (Int16)nPoints).AsEnumerable();
-            //    var dTime = time.ElementAt(1) - time.ElementAt(0);
-            //    foreach (var rho in rhos)
-            //    {
-            //        double[] rOfTime = RofRhoAndT(op.AsEnumerable(), rho.AsEnumerable(), time).ToArray();
-            //        foreach (var ft in fts)
-            //        {
-            //            yield return LinearDiscreteFourierTransform.GetFourierTransform(time.ToArray(), rOfTime, dTime, ft);
-            //        }
-            //    }
-            //}
         }
         
         #endregion Real Domain
@@ -311,7 +327,7 @@ namespace Vts.Modeling.ForwardSolvers
                 foreach (var fx in fxs)
                 {
                     fx_ref = fx * _opReference.Musp / op.Musp;
-                    double exponentialterm = op.Mua * _v * _opReference.Musp / op.Musp;
+                    double exponentialterm = op.Mua * v * _opReference.Musp / op.Musp;
 
                     if (fx_ref <= _sfdGenerator.SpaceValues.MaxValue)
                     {
@@ -380,7 +396,7 @@ namespace Vts.Modeling.ForwardSolvers
 
                             scaledValue = CheckIfValidOutput(scaledValue);
 
-                            yield return scalingFactor * scaledValue * Math.Exp(-op.Mua * _v * t);
+                            yield return scalingFactor * scaledValue * Math.Exp(-op.Mua * v * t);
                         }                   
                     } 
                 }
@@ -401,8 +417,9 @@ namespace Vts.Modeling.ForwardSolvers
         }
         /// <summary>
         /// Evaluates the spatial frequency and temporal frequency resolved reflectance
-        /// calculating the analitycal Fourier transform of the NURBS curve R(t) at the
+        /// calculating the Fourier transform of the NURBS curve R(t) at the
         /// required spatial frequency for the specified optical properties. 
+        /// The computed FT is analitycal or discrete according to the boolean value 'analyticIntegration'.
         /// </summary>
         /// <param name="ops">optical properties</param>
         /// <param name="fxs">spatial frequency</param>
@@ -410,49 +427,63 @@ namespace Vts.Modeling.ForwardSolvers
         /// <returns>spatial frequency and temporal frequancy resolved reflectance</returns>
         public override IEnumerable<Complex> RofFxAndFt(IEnumerable<OpticalProperties> ops, IEnumerable<double> fxs, IEnumerable<double> fts)
         {
+            bool analyticIntegration = false;
             double fx_ref;
             Complex transformedValue;
 
             foreach (var op in ops)
             {
-                foreach (var fx in fxs)
+                if (analyticIntegration)
                 {
-                    fx_ref = fx * _opReference.Musp / op.Musp;
-                    double exponentialterm = op.Mua * _v * _opReference.Musp / op.Musp;
+                    foreach (var fx in fxs)
+                    {
+                        fx_ref = fx * _opReference.Musp / op.Musp;
+                        double exponentialterm = op.Mua * v * _opReference.Musp / op.Musp;
 
-                    if (fx_ref <= _sfdGenerator.SpaceValues.MaxValue)
-                    {
-                        foreach (var ft in fts)
+                        if (fx_ref <= _sfdGenerator.SpaceValues.MaxValue)
                         {
-                            transformedValue = _sfdGenerator.EvaluateNurbsCurveFourierTransform(fx_ref, exponentialterm, ft * _opReference.Musp / op.Musp);
-                            yield return Math.PI * transformedValue;
+                            foreach (var ft in fts)
+                            {
+                                transformedValue = _sfdGenerator.EvaluateNurbsCurveFourierTransform(fx_ref, exponentialterm, ft * _opReference.Musp / op.Musp);
+                                yield return Math.PI * transformedValue;
+                            }
                         }
-                    }
-                    else
-                    {
-                        foreach (var ft in fts)
+                        else
                         {
-                            yield return new Complex(0.0, 0.0);   
+                            foreach (var ft in fts)
+                            {
+                                yield return new Complex(0.0, 0.0);
+                            }
                         }
                     }
                 }
+                else
+                {
+                    var time = _sfdGenerator.TimeKnotSpanPolynomialCoefficients.Select(span => span.GetKnotSpanMidTime());
+                    var deltaT = _sfdGenerator.TimeKnotSpanPolynomialCoefficients.Select(span => span.GetKnotSpanDeltaT());
+
+                    foreach (var fx in fxs)
+                    {
+                        fx_ref = fx * op.Musp / _opReference.Musp;
+                        if (fx_ref <= _sfdGenerator.SpaceValues.MaxValue)
+                        {
+                            var RofT = RofFxAndT(op.AsEnumerable(), fx_ref.AsEnumerable(), time);
+
+                            foreach (var ft in fts)
+                            {
+                                yield return LinearDiscreteFourierTransform.GetFourierTransform(time.ToArray(), RofT.ToArray(), deltaT.ToArray(), ft * _opReference.Musp / op.Musp);    
+                            }
+                        }
+                        else
+                        {
+                            foreach (var ft in fts)
+                            {
+                                yield return new Complex(0.0, 0.0);
+                            }
+                        }
+                    }
+                }  
             }
-            //foreach (var op in ops)
-            //{
-            //    double maxT = _sfdGenerator.TimeValues.MaxValue * _opReference.Musp / op.Musp;
-            //    double maxFt = fts.Last();
-            //    double nPoints = maxT * 2.0 * maxFt;
-            //    var time = new DoubleRange(0.0, maxT, (Int16)nPoints).AsEnumerable();
-            //    var dTime = time.ElementAt(1) - time.ElementAt(0);
-            //    foreach (var fx in fxs)
-            //    {
-            //        double[] rOfTime = RofFxAndT(op.AsEnumerable(), fx.AsEnumerable(), time).ToArray();
-            //        foreach (var ft in fts)
-            //        {
-            //            yield return LinearDiscreteFourierTransform.GetFourierTransform(time.ToArray(), rOfTime, dTime, ft);
-            //        }
-            //    }
-            //}
         }
 
         #endregion Spatial Frequency Domain
@@ -593,7 +624,7 @@ namespace Vts.Modeling.ForwardSolvers
             double slope = (lR2 - lR1) / (deltaT);
             double intercept = -slope * generator.TimeValues.MaxValue + lR1;
             area = -Math.Pow(10.0, intercept + slope * generator.TimeValues.MaxValue)
-                   * Math.Exp(-op.Mua * _v * generator.TimeValues.MaxValue) / (slope - op.Mua);
+                   * Math.Exp(-op.Mua * v * generator.TimeValues.MaxValue) / (slope - op.Mua);
             return area;
         }
         
