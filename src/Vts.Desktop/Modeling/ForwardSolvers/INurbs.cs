32c07673134a09a3ce30be3392e1d7c701cf1586
Michele Martinelli <michelemartinelli32@gmail.com>
2010-07-14 01:23:59 -0700

    created separete files for INurns and for StubNurbsGenerator

---
+++ src/Vts.Desktop/Modeling/ForwardSolvers/INurbs.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vts.IO;
+using Vts.Extensions;
+using Vts.Common;
+using MathNet.Numerics;
+
+namespace Vts.Modeling.ForwardSolvers
+{
+    /// <summary>
+    /// Defines the methods and properties that need to be implemented by the NurbsGenerator 
+    /// class and by its stub version, StubNurbsGenerator used for Unit Testing.  
+    /// </summary>
+    public interface INurbs
+    {
+        /// <summary>
+        /// Defines signature for a method used to consider cousality of the photon migration.
+        /// </summary>
+        /// <param name="rho">source detector separation</param>
+        /// <returns>minimal valid time</returns>
+        double GetMinimumValidTime(double rho);
+
+        /// <summary>
+        /// Defines the signature of the method used to compute a point on a NURBS curve.
+        /// </summary>
+        /// <param name="point">physical coordinate of the point</param>
+        /// <param name="dimension">physical dimension represented by the NURBS curve</param>
+        /// <returns>Value of a point on the curve</returns>
+        double ComputeCurvePoint(double point, NurbsValuesDimensions dimension);
+
+        /// <summary>
+        /// Defines the signature of the method used to compute the point on a NURBS surface.
+        /// </summary>
+        /// <param name="time">time coordinate</param>
+        /// <param name="space">space coordinate(rho or fx)</param>
+        /// <returns>Value of a point  on the surface </returns>
+        double ComputeSurfacePoint(double time, double space);
+
+        /// <summary>
+        /// Defines the signature of the method used to compute the point outside the surface range.
+        /// </summary>
+        /// <param name="time">time coordinate</param>
+        /// <param name="space">space coordinate(rho or fx)</param>
+        /// <param name="edgeValue">point calculated on a position on the limit of the surface</param>
+        /// <returns>Value of a point outside the surface</returns>
+        double ComputePointOutOfSurface(double time, double space, double edgeValue);
+
+        /// <summary>
+        /// Defines the signature of the method used to calculate the integral value of an
+        /// isoparametric NURBS curve multiplied by an exponential function analitically.
+        /// </summary>
+        /// <param name="space">radial position or spatial frequency mapped to the reference spatial value</param>
+        /// <param name="exponentialTerm">exponential decay due to absorption</param>
+        /// <returns>integral value of an isoparametric NURBS curve</returns>
+        double EvaluateNurbsCurveIntegral(double space, double exponentialTerm);
+
+        /// <summary>
+        /// Defines the signature for the method used to evaluate the FT of an isoparametric curve.
+        /// </summary>
+        /// <param name="space">spatial coordinate</param>
+        /// <param name="expTerm">exponential coefficient</param>
+        /// <param name="ft">temporal frequency</param>
+        /// <returns>R(ft) at  fixed rho</returns>
+        Complex EvaluateNurbsCurveFourierTransform(double space, double expTerm, double ft);
+
+        /// <summary>
+        /// Defines the signature of the method used to evaluate the tensor product control points
+        /// necessary to evaluate the integral of an isoparametric curve.
+        /// </summary>
+        /// <param name="space_ref">spatial coordinate mapped to teh reference space</param>
+        /// <returns>Tensor product control points for an isoparametric curve on a surface</returns>
+        List<double[]> EvaluateTensorProductControlPoints(double space_ref);
+
+        /// <summary>
+        /// Gets the NurbsValues along the time dimension.
+        /// </summary>
+        NurbsValues TimeValues { get; }
+
+        /// <summary>
+        /// Gets the NurbsValues along the space dimension.
+        /// </summary>
+        NurbsValues SpaceValues { get; }
+
+        /// <summary>
+        /// Gets or sets the coefficients of the non vanishing B-splines over each knot span.
+        /// </summary>
+        List<BSplinesCoefficients> TimeKnotSpanPolynomialCoefficients { get; set; }
+
+        double[] NativeTimes { get; }
+    }
+}
---
+++ src/Vts/Modeling/ForwardSolvers/StubNurbsGenerator.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Vts.IO;
+using Vts.Extensions;
+using Vts.Common;
+using MathNet.Numerics;
+using Vts.Modeling;
+
+namespace Vts.Modeling.ForwardSolvers
+{
+    /// <summary>
+    /// Class used for Unit testing of the NurbsGenerator methods and of the NurbsForwardSolver
+    /// class.
+    /// </summary>
+    public class StubNurbsGenerator : INurbs
+    {
+        #region INurbs Members
+
+        /// <summary>
+        /// Returns speed of light for n = 1,4.
+        /// </summary>
+        /// <param name="rho">source detector separation</param>
+        /// <returns>v</returns>
+        public double GetMinimumValidTime(double rho)
+        {
+            return rho / 214.0;
+        }
+
+        /// <summary>
+        /// Returns always 1, used for testing.
+        /// </summary>
+        /// <param name="point">point coordinate</param>
+        /// <param name="dimension">dimension</param>
+        /// <returns>1</returns>
+        public double ComputeCurvePoint(double point, NurbsValuesDimensions dimension)
+        {
+            return 1.0;
+        }
+
+        /// <summary>
+        /// Returns always -1, used for testing.
+        /// </summary>
+        /// <param name="time">time coordinate</param>
+        /// <param name="space">space coordinate</param>
+        /// <returns>-1</returns>
+        public double ComputeSurfacePoint(double time, double space)
+        {
+            return -1.0;
+        }
+
+        /// <summary>
+        /// Gets Nurbs values for the time dimensions with max = 1.0
+        /// </summary>
+        public NurbsValues TimeValues
+        {
+            get
+            {
+                NurbsValues timeValues = new NurbsValues(1.0);
+                return timeValues;
+            }
+        }
+
+        /// <summary>
+        /// Gets Nurbs values for the time dimensions with max = 1.0
+        /// </summary>
+        public NurbsValues SpaceValues
+        {
+            get
+            {
+                NurbsValues spaceValues = new NurbsValues(1.0);
+                return spaceValues;
+            }
+        }
+
+
+        /// <summary>
+        /// Not implemented for stub class.
+        /// </summary>
+        public List<BSplinesCoefficients> TimeKnotSpanPolynomialCoefficients
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Not implemented fo stub class.
+        /// </summary>
+        /// <param name="time"></param>
+        /// <param name="space"></param>
+        /// <param name="edgeValue"></param>
+        /// <returns></returns>
+        public double ComputePointOutOfSurface(double time, double space, double edgeValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Not implemented for stub class.
+        /// </summary>
+        /// <param name="space"></param>
+        /// <param name="exponentialTerm"></param>
+        /// <returns></returns>
+        public double EvaluateNurbsCurveIntegral(double space, double exponentialTerm)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Not implemented for stub class.
+        /// </summary>
+        /// <param name="space"></param>
+        /// <param name="expTerm"></param>
+        /// <param name="ft"></param>
+        /// <returns></returns>
+        public Complex EvaluateNurbsCurveFourierTransform(double space, double expTerm, double ft)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double[] NativeTimes
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        List<double[]> INurbs.EvaluateTensorProductControlPoints(double space_ref)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
