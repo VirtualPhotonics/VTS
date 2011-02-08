4f4aa255da78bc16d2718d75c389a900192847e1
Lisa Malenfant <lisa.malenfant@uci.edu>
2011-02-08 15:57:46 -0800

    Added the dll and ClientAccessPolicy files to the web project, updated LibraryIO

--- src/Vts/IO/LibraryIO.cs
+++ src/Vts/IO/LibraryIO.cs
@@ -15,43 +15,61 @@ namespace Vts.IO
     /// </summary>
     public static class LibraryIO
     {
+        private static Assembly _sLDLL;
+        //private static string _sLProjectName;
+        private static List<string> _assemblyList = new List<string>();
+
 #if SILVERLIGHT
-        private static Assembly SLDLL;
-        private static string SLProjectName;
 
         /// <summary>
         /// Loads an assembly from a dll
         /// </summary>
-        /// <param name="pathName">path name and filename of the dll</param>
-        /// <param name="projectName">Type name of the instance (Project name)</param>
-        public static void LoadFromDLL(string pathName, string projectName)
+        /// <param name="FileName">path name and filename of the dll</param>
+        public static void LoadFromDLL(string FileName)
         {
             WebClient downloader = new WebClient(); 
-            string path = pathName;
-            SLProjectName = projectName;
+            string path = FileName.ToString();
             downloader.OpenReadCompleted += new OpenReadCompletedEventHandler(downloader_OpenReadCompleted); 
             downloader.OpenReadAsync(new Uri(path, UriKind.Absolute));
+        }
+
+        public static void PollDLL(string AssemblyName)
+        {
+            bool IsLoaded;
+            do
+            {
+                IsLoaded = IsDLLLoaded(AssemblyName.ToString());
+            } while (!IsLoaded);
         }
 
         static void downloader_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
         {
             AssemblyPart assemblyPart = new AssemblyPart();
-            SLDLL = assemblyPart.Load(e.Result);
-            // SLDLL.CreateInstance(SLProjectName); //what is the scope of this instance?
+            _sLDLL = assemblyPart.Load(e.Result);
+            //Add the current assembly to the list of assemblies
+            _assemblyList.Add(_sLDLL.FullName);
         }
 #else
         /// <summary>
         /// Loads an assembly from a dll
         /// </summary>
-        /// <param name="filename">Path and name of the dll</param>
-        /// <param name="projectName">Type name of the instance (Project Name)</param>
-        /// <returns></returns>
-        public static object LoadFromDLL(string filename, string projectName)
+        /// <param name="FileName">Path and name of the dll</param>
+        public static void LoadFromDLL(string FileName)
         {
-            byte[] bytes = File.ReadAllBytes(filename);
-            Assembly LoadedDLL = Assembly.Load(bytes);
-            return LoadedDLL.CreateInstance(projectName);
+            byte[] bytes = File.ReadAllBytes(FileName);
+            _sLDLL = Assembly.Load(bytes);
+            //return _sLDLL.CreateInstance(SLProjectName);
+            _assemblyList.Add(_sLDLL.FullName);
         }
 #endif
+        public static bool IsDLLLoaded(string AssemblyName)
+        {
+            foreach (string Item in _assemblyList)
+            {
+                if (Item.StartsWith(AssemblyName, StringComparison.CurrentCultureIgnoreCase))
+                    return true;
+            }
+            return false;
+        }
     }
 }
