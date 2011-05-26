c3d2f5ab86c7eb3939761a972af9069655164155
Lisa Malenfant <lisa.malenfant@uci.edu>
2011-05-26 12:37:55 -0700

    Added a readme.txt file to the command-line application and a link to the acknowledgements file. Renamed the default infile.xml from newinfile.xml. Modified the Vts readme.txt file.

--- src/Vts.MonteCarlo.CommandLineApplication/Program.cs
+++ src/Vts.MonteCarlo.CommandLineApplication/Program.cs
@@ -84,7 +84,7 @@ namespace Vts.MonteCarlo.CommandLineApplication
     class MonteCarloSetup
     {
         private string path = "";
-        private string basename = "newinfile";
+        private string basename = "infile";
 
         public bool ValidSimulation { get; set; }
 
@@ -133,8 +133,8 @@ namespace Vts.MonteCarlo.CommandLineApplication
             }
             else
             {
-                Console.WriteLine("\nNo input file specified. Using newinfile.xml from resources... ");
-                Input = SimulationInput.FromFile("newinfile.xml");
+                Console.WriteLine("\nNo input file specified. Using infile.xml from resources... ");
+                Input = SimulationInput.FromFile("infile.xml");
             }
             BatchQuery = Input.AsEnumerable();
             BatchNameQuery = new[] { "" };
@@ -332,7 +332,7 @@ namespace Vts.MonteCarlo.CommandLineApplication
                         new DoubleRange(0.0, 10, 101),
                         new DoubleRange(0.0, Math.PI / 2, 2))
                 });
-            tempInput.ToFile("newinfile.xml");
+            tempInput.ToFile("infile.xml");
 #endif
             #endregion
 
@@ -401,7 +401,7 @@ namespace Vts.MonteCarlo.CommandLineApplication
 
         private static SimulationInput LoadDefaultInputFile()
         {
-            return SimulationInput.FromFileInResources("newinfile.xml", "mc");
+            return SimulationInput.FromFileInResources("infile.xml", "mc");
         }
     }
 }
