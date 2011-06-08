eed5618dbbf5fcbd9cbe26157f685a1b1297ac19
hayakawa <hayakawa@uci.edu>
2011-06-08 14:46:56 -0700

    Added SerializableMersenneTwister and MersenneTwisterSerializationInfo.cs to handle serialization of random number generator for restart purposes. Added unit test to verify saved xml can be read and continue the random sequence in an uninterrupted order.  Added these to Vts.MC.Rng namespace. Unit test passing, but still need to add new code into the MC processing.

--- src/Vts.Test/MonteCarlo/Rng/SerializableMersenneTwisterTests.cs
+++ src/Vts.Test/MonteCarlo/Rng/SerializableMersenneTwisterTests.cs
@@ -2,27 +2,31 @@ using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using Vts.Extensions;
+using Vts.Common;
+using Vts.MonteCarlo.Rng;
 
 namespace Vts.Test.Common
 {
     [TestFixture] 
-    public class RNGExtensionsTests
+    public class SerializableMersenneTwisterTests
     {
-        //[Test]
+        [Test]
         public void validate_saved_random_number_state_is_correct()
         {
             int seed = 0;
             // normal processing
-            var rng =  new MathNet.Numerics.Random.MersenneTwister(seed);
+            var rng = new SerializableMersenneTwister(seed);
             var rng2 = rng.NextDouble();
             var rng3 = rng.NextDouble();
             var rng4 = rng.NextDouble();
             // saved processing
-            RNGExtensions.ToFile(rng, "savedRNG");
-            var savedRNG = RNGExtensions.FromFile("savedRNG");
+            rng.ToFile(rng, "savedRNG.xml");
+            var savedRNG = SerializableMersenneTwister.FromFile("savedRNG.xml");
+            // saved processing next rng
             var savedRNG5 = savedRNG.NextDouble();
+            // normal processing next rng
             var rng5 = rng.NextDouble();
-            //Assert.IsTrue(rng5 == savedRNG5);
+            Assert.IsTrue(rng5 == savedRNG5);
         }
 
     }
