62a8da37fa8113379b39526751cdd98562728ec1
janakarana <none@none>
2011-06-14 16:41:44 -0700

    Fixed issues after merge
    --HG--
    branch : dc-110311-source_restructuring

--- src/Vts.MonteCarlo.CommandLineApplication/Program.cs
+++ src/Vts.MonteCarlo.CommandLineApplication/Program.cs
@@ -256,11 +256,7 @@ namespace Vts.MonteCarlo.CommandLineApplication
                 //null,
                      true, // tally Second Moment
                      0),
-                new CustomPointSourceInputOld(
-                    new Position(0, 0, 0),
-                    new Direction(0, 0, 1),
-                    new DoubleRange(0.0, 0, 1),
-                    new DoubleRange(0.0, 0, 1)),
+                new DirectionalPointSourceInput(),
                 new MultiLayerTissueInput(
                     new LayerRegion[]
                     { 
