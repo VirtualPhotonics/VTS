e766c3e4103136d3ee2825dd004edeab2ff69535
Lisa Malenfant <lisa.malenfant@uci.edu>
2011-06-13 20:08:19 -0700

    Added a test project for the command line application removed an unused using from SpectralDatabaseLoaderTest.cs

---
+++ src/Vts.MonteCarlo.CommandLineApplication.Test/ProgramTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Vts.MonteCarlo.CommandLineApplication;
+
+namespace Vts.MonteCarlo.CommandLineApplication.Test
+{
+    [TestFixture]
+    public class ProgramTest
+    {
+        [Test]
+        public void validate_command_line_no_parameters()
+        {
+            //string[] arguments = new string[0];
+            //CommandLineApplication.Program.Main(arguments);
+            //Assert.Pass();
+        }
+    }
+}
--- src/Vts.Test/Modeling/Spectroscopy/SpectralDatabaseLoaderTest.cs
+++ src/Vts.Test/Modeling/Spectroscopy/SpectralDatabaseLoaderTest.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using System.Windows.Controls;
 using System.IO;
 using NUnit.Framework;
 using Vts.SpectralMapping;
