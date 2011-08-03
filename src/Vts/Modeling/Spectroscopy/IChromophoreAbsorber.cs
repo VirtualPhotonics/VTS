61418d9bdbb3878eab0715caa61de39ff1c9e8cd
Lisa Malenfant <lisa.malenfant@uci.edu>
2011-08-02 17:24:19 -0700

    Updated XML Comments for Spectroscopy Namespace

--- src/Vts/Modeling/Spectroscopy/IChromophoreAbsorber.cs
+++ src/Vts/Modeling/Spectroscopy/IChromophoreAbsorber.cs
@@ -2,14 +2,26 @@ using System.ComponentModel;
 
 namespace Vts.SpectralMapping
 {
+    /// <summary>
+    /// A general interface for a chromophore absorber
+    /// </summary>
     public interface IChromophoreAbsorber : IAbsorber, INotifyPropertyChanged
     {
-        //ChromophoreType ChromophoreType { get; set; }
+        /// <summary>
+        /// The name of the chromophore absorber
+        /// </summary>
         string Name { get; set; }
+        /// <summary>
+        /// The concentration of the chromophore absorber
+        /// </summary>
         double Concentration { get; set; }
+        /// <summary>
+        /// The chromophore coefficient type
+        /// </summary>
         ChromophoreCoefficientType ChromophoreCoefficientType { get; set; }
+        //ChromophoreType ChromophoreType { get; set; }
         //AbsorptionCoefficientUnits AbsorptionCoefficientUnits { get; set; }
-       // ChromDataDistanceUnits DistanceUnits { get; set; }
-      //  ChromDataConcentrationUnits ConcentrationUnits { get; set; }
+        //ChromDataDistanceUnits DistanceUnits { get; set; }
+        //ChromDataConcentrationUnits ConcentrationUnits { get; set; }
     }
 }
