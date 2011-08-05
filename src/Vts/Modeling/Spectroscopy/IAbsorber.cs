fef217f694fa08f820bae6755a200c027b91f8c2
Lisa Malenfant <lisa.malenfant@uci.edu>
2011-08-05 10:23:13 -0700

    Removed the link to Enums.cs from Vts.Desktop Added XML comments for IAbsorber.cs

--- src/Vts/Modeling/Spectroscopy/IAbsorber.cs
+++ src/Vts/Modeling/Spectroscopy/IAbsorber.cs
@@ -1,7 +1,15 @@
 namespace Vts.SpectralMapping
 {
+    /// <summary>
+    /// An interface for an absorber
+    /// </summary>
     public interface IAbsorber
     {
+        /// <summary>
+        /// Returns Mua (absorption coefficient) for a given wavelength
+        /// </summary>
+        /// <param name="wavelength">Wavelength</param>
+        /// <returns>Mua</returns>
         double GetMua(double wavelength);
     }
 }
