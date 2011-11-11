64df9e557813e92df323db683590af84462cc095
Lisa Malenfant <lisa.malenfant@uci.edu>
2011-11-10 17:23:12 -0800

    Added more spectral database tests

--- src/Vts.Test/Modeling/Spectroscopy/SpectralDatabaseTests.cs
+++ src/Vts.Test/Modeling/Spectroscopy/SpectralDatabaseTests.cs
@@ -45,6 +45,10 @@ namespace Vts.Test.Modeling.Spectroscopy
             {
                 _objStore.DeleteFile("dictionary.xml");
             }
+            if (_objStore.FileExists("dictionary2.xml"))
+            {
+                _objStore.DeleteFile("dictionary2.xml");
+            }
             if (_objStore.FileExists("dictionary3.xml"))
             {
                 _objStore.DeleteFile("dictionary3.xml");
@@ -90,6 +94,10 @@ namespace Vts.Test.Modeling.Spectroscopy
             {
                 File.Delete("dictionary.xml");
             }
+            if (File.Exists("dictionary2.xml"))
+            {
+                File.Delete("dictionary2.xml");
+            }
             if (File.Exists("dictionary3.xml"))
             {
                 File.Delete("dictionary3.xml");
@@ -133,7 +141,7 @@ namespace Vts.Test.Modeling.Spectroscopy
         /// validate loading the default spectral database
         /// </summary>
         [Test]
-        public void validate_Loading_Spectral_Database()
+        public void validate_loading_spectral_database()
         {
             var _testDictionary = Vts.SpectralMapping.SpectralDatabase.GetDatabaseFromFile();
             Assert.IsNotNull(_testDictionary);
@@ -143,7 +151,7 @@ namespace Vts.Test.Modeling.Spectroscopy
         /// Validate the serialization of the spectral database
         /// </summary>
         [Test]
-        public void validate_Serializing_Spectral_Database()
+        public void validate_serializing_spectral_database()
         {
             var testDictionary = Vts.SpectralMapping.SpectralDatabase.GetDatabaseFromFile();
             testDictionary.WriteToXML("SpectralDictionary.xml");
@@ -158,7 +166,7 @@ namespace Vts.Test.Modeling.Spectroscopy
         /// Validate the deserialization of the spectral database
         /// </summary>
         [Test]
-        public void validate_Deserializing_Spectral_Database()
+        public void validate_deserializing_spectral_database()
         {
             var testDictionary = Vts.SpectralMapping.SpectralDatabase.GetDatabaseFromFile();
             testDictionary.WriteToXML("dictionary.xml");
@@ -167,10 +175,22 @@ namespace Vts.Test.Modeling.Spectroscopy
         }
 
         /// <summary>
+        /// Validate the data after serializing and deserializing the database
+        /// </summary>
+        [Test]
+        public void validate_serialized_data()
+        {
+            var testDictionary = Vts.SpectralMapping.SpectralDatabase.GetDatabaseFromFile();
+            testDictionary.WriteToXML("dictionary2.xml");
+            var Dvalues = FileIO.ReadFromXML<Dictionary<string, ChromophoreSpectrum>>("dictionary2.xml");
+            Assert.AreEqual(Dvalues["Hb"].Wavelengths[5], testDictionary["Hb"].Wavelengths[5]);
+        }
+
+        /// <summary>
         /// validate loading spectral data from tab-delimited file
         /// </summary>
         [Test]
-        public void validate_Loading_Spectral_Database_from_tsv()
+        public void validate_loading_spectral_database_from_tsv()
         {
             Stream stream = StreamFinder.GetFileStreamFromResources("Modeling/Spectroscopy/Resources/Spectra.txt", "Vts");
 
@@ -215,10 +235,44 @@ namespace Vts.Test.Modeling.Spectroscopy
         }
 
         /// <summary>
+        /// validate loading spectral database and header from tab-delimited file with conversion
+        /// </summary>
+        [Test]
+        public void validate_data_from_tsv()
+        {
+            int linenumber = 5;
+            string line;
+            string[] row;
+
+            Stream stream = StreamFinder.GetFileStreamFromResources("Modeling/Spectroscopy/Resources/Spectra.txt", "Vts");
+            var testDictionary = SpectralDatabase.CreateDatabaseFromFile(stream);
+            stream = StreamFinder.GetFileStreamFromResources("Modeling/Spectroscopy/Resources/Spectra.txt", "Vts");
+            using (StreamReader readFile = new StreamReader(stream))
+            {
+                // read n lines (there is one line of header so
+                for (int i = 0; i <= linenumber; i++)
+                {
+                    readFile.ReadLine();
+                }
+                // get a line from the stream and split the data
+                line = readFile.ReadLine();
+                row = line.Split('\t');
+            }
+            Assert.AreEqual(testDictionary["Hb"].Wavelengths[linenumber], Convert.ToDouble(row[0]));
+            // multiply the value by ln(10)
+            double k =  Math.Log(10);
+            double spectra = Convert.ToDouble(row[1]) * k;
+            // test that the values in the text stream match the ones in the object
+            Assert.AreEqual(testDictionary["HbO2"].Spectrum[linenumber], spectra);
+            spectra = Convert.ToDouble(row[2]) * k;
+            Assert.AreEqual(testDictionary["Hb"].Spectrum[linenumber], spectra);
+        }
+
+        /// <summary>
         /// validate loading spectral database and header from tab-delimited file with no conversion
         /// </summary>
         [Test]
-        public void validate_Loading_Spectral_Database_and_header_from_tsv_no_conversion()
+        public void validate_loading_spectral_database_and_header_from_tsv_no_conversion()
         {
             Stream stream = StreamFinder.GetFileStreamFromResources("Modeling/Spectroscopy/Resources/Spectra.txt", "Vts");
 
