9b5f0fd320706adcbd6bf2d044a982d7890de006
dcuccia <none@none>
2012-01-17 18:19:33 -0800

    Minor edits removing comments and adding help capability to post-processor

--- src/Vts.MonteCarlo.CommandLineApplication/MonteCarloSetup.cs
+++ src/Vts.MonteCarlo.CommandLineApplication/MonteCarloSetup.cs
@@ -117,8 +117,6 @@ namespace Vts.MonteCarlo.CommandLineApplication
             {
                 var sweepValues = parameterSweep.Range.AsEnumerable();
 
-                // todo: make more dynamic/flexible by removing requirment for enum value, and instead rely on parsing string name and index djc 2011-06-03
-                // var inputParameterType = (InputParameterType)Enum.Parse(typeof(InputParameterType), parameterSweep.Name, true);
                 batchInputs = batchInputs.WithParameterSweep(sweepValues, parameterSweep.Name.ToLower());
             }
 
