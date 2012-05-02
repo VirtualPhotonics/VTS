df4f2f6ae34b56ee5d1c9dcf1af0e4410bf41441
Lisa Malenfant <lisa.malenfant@uci.edu>
2012-05-01 22:50:47 -0700

    Updates to the batch files

--- SilverlightBuild.bat
+++ SilverlightBuild.bat
@@ -1,14 +1,17 @@
 set rootdir=%~dp0
 set debugbuildswitches=/p:WarningLevel=2 /nologo /v:n
 set releasebuildswitches=/p:Configuration=Release /p:WarningLevel=2 /nologo /v:n
+set releaseWLbuildswitches=/p:Configuration=Release (White List Gui Deployment) /p:WarningLevel=2 /nologo /v:n
 
 PATH=%PATH%;%WINDIR%\Microsoft.Net\Framework\v4.0.30319
 
 msbuild "%rootdir%\src\Vts\Vts.csproj" %debugbuildswitches%
 msbuild "%rootdir%\src\Vts\Vts.csproj" %releasebuildswitches%
+msbuild "%rootdir%\src\Vts\Vts.csproj" %releaseWLbuildswitches%
 
 msbuild "%rootdir%\src\Vts.SiteVisit\Vts.SiteVisit.csproj" %debugbuildswitches%
 msbuild "%rootdir%\src\Vts.SiteVisit\Vts.SiteVisit.csproj" %releasebuildswitches%
+msbuild "%rootdir%\src\Vts.SiteVisit\Vts.SiteVisit.csproj" %releaseWLbuildswitches%
 
 msbuild "%rootdir%\src\Vts.Test\Vts.Test.csproj" %debugbuildswitches%
 msbuild "%rootdir%\src\Vts.Test\Vts.Test.csproj" %releasebuildswitches%
--- src/Vts.MonteCarlo.CommandLineApplication/CreateRelease.bat
+++ src/Vts.MonteCarlo.CommandLineApplication/CreateRelease.bat
@@ -1,25 +1,26 @@
 @echo off
+set currentdir=%~dp0
 
-set currentdir=%CD%
+chdir %currentdir%
+cd ..\..
 
-chdir ..\..
-
-set archive=%currentdir%\MC_v1.0.4Beta.zip
+set zip=%CD%\Tools\7zip\
+set archive=%CD%\build\MC_v1.0.4Beta.zip
 
 set targetdir=%CD%\build\apps\mc\Release\
 set targetmatlabdir=%CD%\matlab\post_processing\monte_carlo\simulation_result_loading\
 
 
-"%currentdir%"\7za a "%archive%" "%targetdir%"mc.exe
+"%zip%"7za a "%archive%" "%targetdir%"mc.exe
 
-"%currentdir%"\7za a "%archive%" "%targetdir%"*.dll
+"%zip%"7za a "%archive%" "%targetdir%"*.dll
 
-"%currentdir%"\7za a "%archive%" "%targetdir%"*.txt
+"%zip%"7za a "%archive%" "%targetdir%"*.txt
 
-"%currentdir%"\7za a "%archive%" "%targetdir%"*.config
+"%zip%"7za a "%archive%" "%targetdir%"*.config
 
-"%currentdir%"\7za a "%archive%" "%targetdir%"infile_*.xml
+"%zip%"7za a "%archive%" "%targetdir%"infile_*.xml
 
-"%currentdir%"\7za a -r "%archive%" "%targetmatlabdir%"*.*
+"%zip%"7za a -r "%archive%" "%targetmatlabdir%"*.*
 
 rem pause
