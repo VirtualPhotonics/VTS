0d2c537e6dfce592bb7c677e7e578d9f94a9f8b3
Lisa Malenfant <lisa.malenfant@uci.edu>
2012-05-24 14:10:30 -0700

    Added a test for serializing MultiLayerTissueInput
    --HG--
    branch : mono-compatibility

---
+++ src/Vts.Test/MonteCarlo/DataStructures/TissueInputs/MultiLayerTissueInputTests.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using NUnit.Framework;
+using Vts.Common;
+using Vts.MonteCarlo;
+using Vts.MonteCarlo.Tissues;
+using Vts.IO;
+
+namespace Vts.Test.MonteCarlo
+{
+    [TestFixture]
+    public class MultiLayerTissueInputTests
+    {
+        [Test]
+        public void validate_deserialized_class_is_correct()
+        {
+            var i = new MultiLayerTissueInput(new ITissueRegion[]
+                    { 
+                        new LayerRegion(
+                            new DoubleRange(double.NegativeInfinity, 0.0),
+                            new OpticalProperties(0.0, 1e-10, 1.0, 1.0)),
+                        new LayerRegion(
+                            new DoubleRange(0.0, 100.0),
+                            new OpticalProperties(0.01, 1.0, 0.8, 1.4)),
+                        new LayerRegion(
+                            new DoubleRange(100.0, double.PositiveInfinity),
+                            new OpticalProperties(0.0, 1e-10, 1.0, 1.0))
+                    }
+                );
+
+            var iCloned = Clone(i);
+
+            Assert.AreEqual(iCloned.Regions[1].RegionOP.Mus, i.Regions[1].RegionOP.Mus);
+        }
+
+        [Test]
+        public void validate_deserialized_class_is_correct_when_using_FileIO()
+        {
+            var i = new MultiLayerTissueInput(new ITissueRegion[]
+                    { 
+                        new LayerRegion(
+                            new DoubleRange(double.NegativeInfinity, 0.0),
+                            new OpticalProperties(0.0, 1e-10, 1.0, 1.0)),
+                        new LayerRegion(
+                            new DoubleRange(0.0, 100.0),
+                            new OpticalProperties(0.01, 1.0, 0.8, 1.4)),
+                        new LayerRegion(
+                            new DoubleRange(100.0, double.PositiveInfinity),
+                            new OpticalProperties(0.0, 1e-10, 1.0, 1.0))
+                    }
+                );
+            i.WriteToXML("MultiLayerTissue.xml");
+            var iCloned = FileIO.ReadFromXML<MultiLayerTissueInput>("MultiLayerTissue.xml");
+
+            Assert.AreEqual(iCloned.Regions[1].RegionOP.Mus, i.Regions[1].RegionOP.Mus);
+        }
+
+        private static T Clone<T>(T myObject)
+        {
+            using (MemoryStream ms = new MemoryStream(1024))
+            {
+                var dcs = new DataContractSerializer(typeof(T));
+                dcs.WriteObject(ms, myObject);
+                ms.Seek(0, SeekOrigin.Begin);
+                return (T)dcs.ReadObject(ms);
+            }
+        }
+
+    }
+}
