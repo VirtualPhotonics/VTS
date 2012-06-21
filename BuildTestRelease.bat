66e783160d2ec609b905f79083c91f7633b45bf6
Lisa Malenfant <lisa.malenfant@uci.edu>
2012-06-21 12:23:56 -0700

    Updated the batch file to change the current directory to the location of the batch file so the unit test XML file is created here

--- BuildTestRelease.bat
+++ BuildTestRelease.bat
@@ -1,5 +1,9 @@
+rem ********** RUN THIS FILE AS ADMINISTRATOR **********
 set rootdir=%~dp0
 set version=1.0.5
+
+rem ********** CHANGE CURRENT DIR TO LOCATION OF BAT FILE **********
+cd %~dp0
 
 rem ********** CLEAN THE MC RELEASE FOLDERS **********
 rmdir "%rootdir%build" /s /q
@@ -9,8 +13,6 @@ rmdir "%rootdir%matlab\vts_wrapper\vts_libraries" /s /q
 
 rem ********** BUILD THE DESKTOP VERSION **********
 call "%rootdir%DesktopBuild.bat"
-
-rem ********** RUN DESKTOP UNIT TESTS **********
 call "%rootdir%DesktopTests.bat"
 
 
@@ -21,12 +23,10 @@ call "%rootdir%matlab\CreateRelease.bat" %version%
 rem ********** BUILD THE SILVERLIGHT VERSION **********
  call "%rootdir%SilverlightBuild.bat"
 
-rem ********** RUN SILVERLIGHT UNIT TESTS **********
 cd "%rootdir%src\Vts.Test\bin\Debug\"
 start TestPage.html
 
 pause
 
-rem ********** RUN SILVERLIGHT GUI (WHITE LIST VERSION) **********
-cd "%rootdir%src\Vts.SiteVisit\bin\ReleaseWhiteList\"
+cd "%rootdir%src\Vts.SiteVisit\bin\Debug\"
 start TestPage.html
