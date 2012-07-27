e2b60cc5c3770b9de56738afc8c96da31ab721ae
Lisa Malenfant <lisa.malenfant@uci.edu>
2012-07-26 20:55:19 -0700

    Updated the version number

--- BuildTestRelease.bat
+++ BuildTestRelease.bat
@@ -1,6 +1,6 @@
 rem ********** RUN THIS FILE AS ADMINISTRATOR **********
 set rootdir=%~dp0
-set version=1.0.5
+set version=1.0.6
 
 rem ********** CHANGE CURRENT DIR TO LOCATION OF BAT FILE **********
 cd %~dp0
