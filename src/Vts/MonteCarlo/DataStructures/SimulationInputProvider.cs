8a5ac6432b68def42355e0063be6545746713265
hayakawa <hayakawa@uci.edu>
2012-10-19 13:46:10 -0700

    Added Fluence(r,z,time) to SimulationInputProvider.

--- src/Vts/MonteCarlo/DataStructures/SimulationInputProvider.cs
+++ src/Vts/MonteCarlo/DataStructures/SimulationInputProvider.cs
@@ -101,6 +101,10 @@ namespace Vts.MonteCarlo
                     new FluenceOfRhoAndZDetectorInput(                            
                         new DoubleRange(0.0, 10, 101),
                         new DoubleRange(0.0, 10, 101)),
+                    new FluenceOfRhoAndZAndTimeDetectorInput(
+                        new DoubleRange(0.0, 10, 101),
+                        new DoubleRange(0.0, 10, 101), 
+                        new DoubleRange(0.0, 10, 101)),
                     new FluenceOfXAndYAndZDetectorInput(
                         new DoubleRange(-10, 10, 201),
                         new DoubleRange(-10, 10, 2),
