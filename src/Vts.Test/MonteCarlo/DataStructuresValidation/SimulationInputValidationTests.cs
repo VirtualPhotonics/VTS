477605a23a29fc867a19bc166c3989bb946e8a06
hayakawa <hayakawa@uci.edu>
2013-01-10 19:49:42 -0800

    Added SimulationInputValidation check to make sure if an angled source is defined, that cylindrical detectors are not defined.  Added unit test to verify new code.

--- src/Vts.Test/MonteCarlo/DataStructuresValidation/SimulationInputValidationTests.cs
+++ src/Vts.Test/MonteCarlo/DataStructuresValidation/SimulationInputValidationTests.cs
@@ -1,7 +1,7 @@
-using System.IO;
+using System;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
 using NUnit.Framework;
+using Vts.Common;
 using Vts.MonteCarlo;
 
 namespace Vts.Test.MonteCarlo.DataStructuresValidation
@@ -40,6 +40,19 @@ namespace Vts.Test.MonteCarlo.DataStructuresValidation
             {
                 Options = new SimulationOptions() { AbsorptionWeightingType = AbsorptionWeightingType.Continuous},
                 DetectorInputs = new List<IDetectorInput> { new RadianceOfXAndYAndZAndThetaAndPhiDetectorInput()}
+            };
+            var result = SimulationInputValidation.ValidateInput(input);
+            Assert.IsFalse(result.IsValid);
+        }
+        [Test]
+        public void validate_angled_source_and_cylindrical_detectors_are_not_defined_together()
+        {
+            // generate input with angled source and cylindrical detector
+            var input = new SimulationInput()
+            {
+                SourceInput = new DirectionalPointSourceInput(
+                    new Position(0,0,0), new Direction(1.0/Math.Sqrt(2), 0, 1.0/Math.Sqrt(2)),1),
+                DetectorInputs = new List<IDetectorInput> { new ROfRhoDetectorInput() }
             };
             var result = SimulationInputValidation.ValidateInput(input);
             Assert.IsFalse(result.IsValid);
