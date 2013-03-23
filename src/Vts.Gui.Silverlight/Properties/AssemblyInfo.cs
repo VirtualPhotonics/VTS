8c40735314080496b9244f73caba0c4e0e32adab
dcuccia <none@none>
2013-03-23 13:10:55 -0700

    Added FloatableWindow to our SL source tree (hasn't changed for 4 years)
    --HG--
    branch : dc-130321-nuget_integration

--- src/Vts.Gui.Silverlight/Properties/AssemblyInfo.cs
+++ src/Vts.Gui.Silverlight/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows.Markup;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -13,6 +14,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2012 Laser Microbeam and Medical Program")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: XmlnsPrefix("clr-namespace:System.Windows.Controls;assembly=FloatableWindow", "windows")]
+[assembly: XmlnsDefinition("clr-namespace:System.Windows.Controls;assembly=FloatableWindow", "System.Windows.Controls")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
