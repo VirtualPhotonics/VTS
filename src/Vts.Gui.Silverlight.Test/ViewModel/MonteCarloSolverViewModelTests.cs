8c48228856df7254a7f1bf41ec22f14c3fa61348
hayakawa <hayakawa@uci.edu>
2014-06-19 14:23:51 -0700

    GUI test: kept MCSolverVeiwModelTests class but commented out unit test to verify download of sample infiles (plan to test this in a human way).

--- src/Vts.Gui.Silverlight.Test/ViewModel/MonteCarloSolverViewModelTests.cs
+++ src/Vts.Gui.Silverlight.Test/ViewModel/MonteCarloSolverViewModelTests.cs
@@ -14,11 +14,12 @@ namespace Vts.Gui.Silverlight.Test.ViewModel
             _vm = new MonteCarloSolverViewModel();
         }
 
-        [Test]
-        public void download_prototype_infiles_works_correctly()
-        {
-            _vm.DownloadDefaultSimulationInputCommand.Execute(null);
-        }
+        // following test needs MVVM Light references and user interaction so will be tested in a human way
+        //[Test]
+        //public void download_prototype_infiles_works_correctly()
+        //{
+        //    _vm.DownloadDefaultSimulationInputCommand.Execute(null);
+        //}
 
     }
 }
