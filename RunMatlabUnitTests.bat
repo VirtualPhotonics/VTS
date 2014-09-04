b68c5b39dd0a1587584b2a7741bea2a12dd78cf5
hayakawa <hayakawa@uci.edu>
2014-09-03 19:21:39 -0700

    Edits to the RunMatlabUnitTests.bat to: 1) write matlab interop MC tests to "results*" folders so that they are easily removed prior to creating zip, 2) run MC with infile_one_layer_all_detectors.txt and run results through matlab MC post-processing code.  Currently this batch file is not uncommented in BuildTestRelease.bat.  When thoroughly checked out, will be uncommented.

--- RunMatlabUnitTests.bat
+++ RunMatlabUnitTests.bat
@@ -1,23 +1,29 @@
 rem ********** RUN THIS FILE AS ADMINISTRATOR **********
 set rootdir=%~dp0
-set version=2.0.0
-set EnableNuGetPackageRestore=true
-
-rem ********** CHANGE CURRENT DIR TO LOCATION OF BAT FILE **********
-cd %~dp0
 
 rem ********** CHANGE CURRENT DIR TO MATLAB VTS_WRAPPER DIRECTORY **********
 cd "%rootdir%matlab\vts_wrapper"
 
 rem ********** RUN VTS_WRAPPER TESTS **********
-matlab -r "vts_tests; quit" -logfile vts_wrapper_logfile
+rem ********** MATLAB NOT EXITING NICELY INDICATES TEST FAILURE **********
+matlab -r "vts_tests; quit" 
+#matlab -r "mc_tests; structures_tests; quit" # to only run unit tests
 
-# could add code to check logfile but if anything errors in unit tests,
-# the "quit" will not get invoked and matlab will not close
 
+rem ********** CHANGE CURRENT DIR TO VTS.MONTECARLO.COMMANDLINEAPPLICATION/BIN/RELEASE DIRECTORY  **********
+cd "%rootdir%src\Vts.MonteCarlo.CommandLineApplication\bin\Release"
 
-#rem ********** CHANGE CURRENT DIR TO BUILD DIRECTORY AND EXTRACT MC ZIP **********
-#cd "%rootdir%build"
+rem ********** RUN MONTE CARLO WITH GENERAL INFILE  **********
+mc.exe infile=infile_one_layer_all_detectors.txt
 
-# would like to run MC with infile_one_layer_all_detectors.txt and then
-# run load_results_script.m 
+rem ********** CHANGE CURRENT DIR TO MATLAB MONTE CARLO POST-PROCESSING DIRECTORY **********
+cd "%rootdir%matlab\post_processing\monte_carlo\simulation_result_loading"
+
+rem ********** COPY RESULTS FROM MONTE CARLO TO CURRENT DIRECTORY **********
+# remove any residual folder
+rmdir "%rootdir%matlab\post_processing\monte_carlo\simulation_result_loading\one_layer_all_detectors" /s /q 
+xcopy "%rootdir%src\Vts.MonteCarlo.CommandLineApplication\bin\Release\one_layer_all_detectors\*" "%rootdir%matlab\post_processing\monte_carlo\simulation_result_loading\one_layer_all_detectors\"
+
+rem ********** RUN LOAD_RESULTS_SCRIPT (DEFAULT DATANAMES IS SET TO one_layer_all_detectors) **********
+rem ********** MATLAB NOT EXITING NICELY INDICATES TEST FAILURE **********
+matlab -r "load_results_script; quit"
