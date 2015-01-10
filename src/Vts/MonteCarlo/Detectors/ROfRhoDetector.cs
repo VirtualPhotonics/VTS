3f0f01e3d04fd19d60e6fc2d489ce16b6dcd2456
hayakawa <hayakawa@uci.edu>
2015-01-10 09:39:31 -0800

    MC: modified 1) photon.CrossRegionOrReflect to now update direction cosines upon exiting tissue, 2) R(rho) to have property FinalTissueRegionIndex for user to set what region detector resides, and added logic to IsWithinDetectorAperture to check detector NA and backup one collision if FinalTissueRegionIndex is not CurrentRegionIndex. All unit tests passing.
    --HG--
    branch : ch-141202-detector-NA-changes

--- src/Vts/MonteCarlo/Detectors/ROfRhoDetector.cs
+++ src/Vts/MonteCarlo/Detectors/ROfRhoDetector.cs
@@ -212,7 +212,7 @@ namespace Vts.MonteCarlo.Detectors
             else // determine n of prior tissue region
             {
                 var detectorRegionN = _tissue.Regions[FinalTissueRegionIndex].RegionOP.N;
-                return photon.History.PreviousDP.IsWithinNA(NA, Direction.AlongNegativeZAxis, detectorRegionN);
+                return photon.History.PreviousDP.IsWithinNA(NA, Direction.AlongPositiveXAxis, detectorRegionN);
             }
             //return true; // or, possibly test for NA or confined position, etc
             //return (dp.StateFlag.Has(PhotonStateType.PseudoTransmissionDomainTopBoundary));
