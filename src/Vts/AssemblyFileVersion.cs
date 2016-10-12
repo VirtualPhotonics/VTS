7a74e63b322aa56b2d0f1a36e99d34b3e87d9b6a
hayakawa <hayakawa@uci.edu>
2016-10-12 10:34:29 -0700

    version change in preparation for Codeplex new releases.

--- src/Vts/AssemblyFileVersion.cs
+++ src/Vts/AssemblyFileVersion.cs
@@ -10,4 +10,4 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyFileVersion("1.0.*")]
-[assembly: AssemblyFileVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.2.0")]
