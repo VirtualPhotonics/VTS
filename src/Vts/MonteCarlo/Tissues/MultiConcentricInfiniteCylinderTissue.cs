ec0ac84cedd05c438ad6bfb2a8d05722b3591d45
hayakawa <hayakawa@uci.edu>
2019-08-16 10:50:18 -0700

    MC: unfinished start at concentric cylinder tissue definition.

---
+++ src/Vts/MonteCarlo/Tissues/MultiConcentricInfiniteCylinderTissue.cs
@@ -0,0 +1,289 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using Vts.Common;
+
+namespace Vts.MonteCarlo.Tissues
+{
+    /// <summary>
+    /// Implements ITissueInput.  Defines input to MultiConcentricInfiniteCylinderTissue class.
+    /// This assumes infinite cylinders are concentric and lie entirely within a layer.
+    /// </summary>
+    public class MultiConcentricInfiniteCylinderTissueInput : TissueInput, ITissueInput
+    {
+        private ITissueRegion[] _regions;
+        private ITissueRegion[] _layerRegions;
+        private ITissueRegion[] _infiniteCylinderRegions;
+
+        /// <summary>
+        /// constructor for Multi-ConcentricInfiniteCylinder tissue input
+        /// </summary>
+        /// <param name="infiniteCylinderRegions">concentric cylinder regions, larger radius first</param>
+        /// <param name="layerRegions">layer regions</param>
+        public MultiConcentricInfiniteCylinderTissueInput(
+            ITissueRegion[] infiniteCylinderRegions,
+            ITissueRegion[] layerRegions)
+        {
+            TissueType = "MultiConcentricInfiniteCylinder";
+            _layerRegions = layerRegions;
+            _infiniteCylinderRegions = infiniteCylinderRegions;
+        }
+
+        /// <summary>
+        /// MultiConcentricInfiniteCylinderTissue default constructor provides homogeneous tissue
+        /// </summary>
+        public MultiConcentricInfiniteCylinderTissueInput()
+            : this(
+                new ITissueRegion[]
+                {
+                    new InfiniteCylinderTissueRegion(
+                        new Position(0, 0, 1),
+                        0.75,
+                        new OpticalProperties(0.05, 1.0, 0.8, 1.4)
+                    ),
+                    new InfiniteCylinderTissueRegion(
+                        new Position(0, 0, 1),
+                        0.5,
+                        new OpticalProperties(0.05, 1.0, 0.8, 1.4)
+                    )
+                },
+                new ITissueRegion[]
+                { 
+                    new LayerTissueRegion(
+                        new DoubleRange(double.NegativeInfinity, 0.0),
+                        new OpticalProperties( 0.0, 1e-10, 1.0, 1.0)),
+                    new LayerTissueRegion(
+                        new DoubleRange(0.0, 100.0),
+                        new OpticalProperties(0.0, 1.0, 0.8, 1.4)),
+                    new LayerTissueRegion(
+                        new DoubleRange(100.0, double.PositiveInfinity),
+                        new OpticalProperties(0.0, 1e-10, 1.0, 1.0))
+                })
+        {
+        }
+
+        /// <summary>
+        /// list of tissue regions comprising tissue
+        /// </summary>
+        [IgnoreDataMember]
+        public ITissueRegion[] Regions { get { return _layerRegions.Concat(_infiniteCylinderRegions).ToArray(); } }
+        /// <summary>
+        /// tissue outer infinite cylinder region
+        /// </summary>
+        public ITissueRegion[] InfiniteCylinderRegions { get { return _infiniteCylinderRegions; } set { _infiniteCylinderRegions = value; } }
+         /// <summary>
+        /// tissue layer regions
+        /// </summary>
+        public ITissueRegion[] LayerRegions { get { return _layerRegions; } set { _layerRegions = value; } }
+
+        /// <summary>
+        /// Required factory method to create the corresponding 
+        /// ITissue based on the ITissueInput data
+        /// </summary>
+        /// <param name="awt">Absorption Weighting Type</param>
+        /// <param name="pft">Phase Function Type</param>
+        /// <param name="russianRouletteWeightThreshold">Russian Roulette Weight Threshold</param>
+        /// <returns></returns>
+        public ITissue CreateTissue(AbsorptionWeightingType awt, PhaseFunctionType pft, double russianRouletteWeightThreshold)
+        {
+            var t = new MultiConcentricInfiniteCylinderTissue(Regions);
+
+            t.Initialize(awt, pft, russianRouletteWeightThreshold);
+
+            return t;
+        }
+    }
+
+    /// <summary>
+    /// Implements ITissue.  Defines tissue geometries comprised of layers
+    /// (including homogenous with air layers above and below).  Layers are infinite along
+    /// x- and y- axes.
+    /// </summary>
+    public class MultiConcentricInfiniteCylinderTissue : TissueBase, ITissue
+    {
+        private IList<LayerTissueRegion> _layerRegions;
+        private IList<InfiniteCylinderTissueRegion> _infiniteCylinderRegions;
+        private int _inclusionRegionIndex;
+
+        /// <summary>
+        /// Creates an instance of a MultiConcentricInfiniteCylinderTissue
+        /// </summary>
+        /// <param name="regions">list of tissue regions comprising tissue</param>
+        /// <remarks>air above and below tissue needs to be specified for a slab geometry</remarks>
+        public MultiConcentricInfiniteCylinderTissue(
+            IList<ITissueRegion> regions)
+            : base(regions)
+        {
+            _layerRegions = regions.Select(region => (LayerTissueRegion) region).ToArray();
+            _infiniteCylinderRegions = regions.Select(region => (InfiniteCylinderTissueRegion) region).ToArray();
+            _inclusionRegionIndex = _layerRegions.Count; // index is, by convention, after the layer region indices
+            // also by convention larger radius infinite cylinder is first
+        }
+
+        /// <summary>
+        /// Creates a default instance of a MultiConcentricInfiniteCylinderTissue based on a homogeneous medium slab geometry
+        /// and discrete absorption weighting
+        /// </summary>
+        public MultiConcentricInfiniteCylinderTissue() 
+            : this(new MultiConcentricInfiniteCylinderTissueInput().Regions)
+        {
+        }
+
+        /// <summary>
+        /// method to determine region index of region photon is currently in
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        public virtual int GetRegionIndex(Position position)
+        {
+            // first use LayerTissueRegion to determine which region photon resides
+            int index = -1;
+            for (int i = 0; i < _layerRegions.Count(); i++)
+            {
+                if (_layerRegions[i].ContainsPosition(position))
+                {
+                    index = i;
+                }
+            }
+            // then use InfiniteTissueRegion to determine if within one of cylinders
+            for (int j = 0; j < _infiniteCylinderRegions.Count(); j++)
+            {
+                if (_infiniteCylinderRegions[j].ContainsPosition(position))
+                {
+                    index = j;
+                }
+            }
+            return index;
+        }
+        
+        /// <summary>
+        /// Finds the distance to the next boundary and independent of hitting it
+        /// </summary>
+        /// <param name="photon"></param>
+        public virtual double GetDistanceToBoundary(Photon photon)
+        {
+            if (photon.DP.Direction.Uz == 0.0)
+            {
+                return double.PositiveInfinity;
+            }
+
+            // going "up" in negative z-direction
+            bool goingUp = photon.DP.Direction.Uz < 0.0;
+
+            // get current and adjacent regions
+            int currentRegionIndex = photon.CurrentRegionIndex; 
+            // check if in embedded tissue region ckh fix 8/10/11
+            LayerTissueRegion currentRegion = _layerRegions[1];
+            if (currentRegionIndex < _layerRegions.Count)
+            {
+                currentRegion = _layerRegions[currentRegionIndex];
+            }
+
+            // calculate distance to boundary based on z-projection of photon trajectory
+            double distanceToBoundary =
+                goingUp
+                    ? (currentRegion.ZRange.Start - photon.DP.Position.Z) / photon.DP.Direction.Uz
+                    : (currentRegion.ZRange.Stop - photon.DP.Position.Z) / photon.DP.Direction.Uz;
+
+
+            return distanceToBoundary;
+        }
+        /// <summary>
+        /// method to determine if on boundary of tissue, i.e. at tissue/air interface
+        /// </summary>
+        /// <param name="position">photon position</param>
+        /// <returns></returns>
+        public virtual bool OnDomainBoundary(Position position)
+        {
+            // this code assumes that the first and last layer is air
+            return 
+                position.Z < 1e-10 ||
+                (Math.Abs(position.Z - (_layerRegions.Last()).ZRange.Start) < 1e-10);
+        }
+        /// <summary>
+        /// method to determine index of region photon is about to enter
+        /// </summary>
+        /// <param name="photon">photon info including position and direction</param>
+        /// <returns>region index</returns>
+        public virtual int GetNeighborRegionIndex(Photon photon)
+        {
+            if (photon.DP.Direction.Uz == 0.0)
+            {
+                throw new Exception("GetNeighborRegionIndex called and Photon not on boundary");
+            }
+
+            if (photon.DP.Direction.Uz > 0.0)
+            {
+                return Math.Min(photon.CurrentRegionIndex + 1, Regions.Count - 1);
+            }
+                
+            return Math.Max(photon.CurrentRegionIndex - 1, 0);
+        }
+        /// <summary>
+        /// method to determine photon state type of photon exiting tissue boundary
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        public PhotonStateType GetPhotonDataPointStateOnExit(Position position)
+        {
+            if (position.Z < 1e-10)
+            {
+                return PhotonStateType.PseudoReflectedTissueBoundary;
+            }
+            
+            return PhotonStateType.PseudoTransmittedTissueBoundary;
+        }
+        /// <summary>
+        /// method to determine direction of reflected photon
+        /// </summary>
+        /// <param name="positionCurrent"></param>
+        /// <param name="directionCurrent"></param>
+        /// <returns></returns>
+        public virtual Direction GetReflectedDirection(
+            Position positionCurrent, 
+            Direction directionCurrent)
+        {
+            return new Direction(
+                directionCurrent.Ux,
+                directionCurrent.Uy,
+                -directionCurrent.Uz);
+        }
+        /// <summary>
+        /// method to determine refracted direction of photon
+        /// </summary>
+        /// <param name="positionCurrent">current photon position</param>
+        /// <param name="directionCurrent">current photon direction</param>
+        /// <param name="nCurrent">refractive index of current region</param>
+        /// <param name="nNext">refractive index of next region</param>
+        /// <param name="cosThetaSnell">cos(theta) resulting from Snell's law</param>
+        /// <returns>direction</returns>
+        public virtual Direction GetRefractedDirection(
+            Position positionCurrent, 
+            Direction directionCurrent, 
+            double nCurrent, 
+            double nNext, 
+            double cosThetaSnell)
+        {
+            if (directionCurrent.Uz > 0)
+                return new Direction(
+                    directionCurrent.Ux * nCurrent / nNext,
+                    directionCurrent.Uy * nCurrent / nNext,
+                    cosThetaSnell);
+            else
+                return new Direction(
+                    directionCurrent.Ux * nCurrent / nNext,
+                    directionCurrent.Uy * nCurrent / nNext,
+                    -cosThetaSnell);
+        }
+        /// <summary>
+        /// method to get cosine of the angle between photons current direction and boundary normal
+        /// </summary>
+        /// <param name="photon"></param>
+        /// <returns>Uz=cos(theta)</returns>
+        public double GetAngleRelativeToBoundaryNormal(Photon photon)
+        {
+            return Math.Abs(photon.DP.Direction.Uz); // abs will work for upward normal and downward normal
+        }
+    }
+}
