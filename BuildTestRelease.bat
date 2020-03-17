ec0c30c5b5112a2961682c99d165a58157b2dccc
Lisa Malenfant <lisa@silverliningcomputing.com>
2020-03-17 10:38:30 -0700

    Updated the NuGet packages and created a new release

--- BuildTestRelease.bat
+++ BuildTestRelease.bat
@@ -1,6 +1,6 @@
 rem ********** RUN THIS FILE AS ADMINISTRATOR **********
 set rootdir=%~dp0
-set version=4.8.0
+set version=4.9.0
 set EnableNuGetPackageRestore=true
 
 rem ********** CHANGE CURRENT DIR TO LOCATION OF BAT FILE **********
