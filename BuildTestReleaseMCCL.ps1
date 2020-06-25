01c9e32d1c3d2eacc81a9170e7835b1b9ba07a8a
hayakawa <hayakawa@uci.edu>
2020-06-25 15:31:51 -0700

    Uncommented code and modification to get a local executable in \publish\local to run "./mc geninfiles"

--- BuildTestReleaseMCCL.ps1
+++ BuildTestReleaseMCCL.ps1
@@ -26,28 +26,20 @@ Write-Host "Run Vts.Test Debug and Release" -ForegroundColor Green
 dotnet test $PWD\src\Vts.Test\Vts.Test.csproj -c Debug
 dotnet test $PWD\src\Vts.Test\Vts.Test.csproj -c Release
 
-Write-Host "Release Packages" -ForegroundColor Green
-Write-Host "Clean Release folders" -ForegroundColor Green
-Remove-Item "$PWD/release" -Recurse -ErrorAction Ignore
-if (Test-Path $PWD\publish) {
-  Remove-Item $PWD\publish -Recurse -ErrorAction Ignore
-}
-New-Item -Path $PWD -Name ".\publish\win-x64" -ItemType "directory"
-New-Item -Path $PWD -Name ".\publish\linux-x64" -ItemType "directory"
-New-Item -Path $PWD -Name ".\publish\osx-x64" -ItemType "directory"
+Write-Host "Release Packages: version = $version" -ForegroundColor Green
 
-# run next 2 line prior to RunMATLABUnitTests to setup up publish with results
-dotnet build $mcclcsproj -c Release
+Write-Host "Create clean publish and release folders" -ForegroundColor Green
+Remove-Item "$PWD\release" -Recurse -ErrorAction Ignore
+Remove-Item $PWD\publish -Recurse -ErrorAction Ignore
+
 dotnet publish $mcclcsproj -c Release -r linux-x64 -o $PWD\publish\linux-x64 --self-contained false 
 dotnet publish $mcclcsproj -c Release -r win-x64 -o $PWD\publish\win-x64 --self-contained false 
 dotnet publish $mcclcsproj -c Release -r osx-x64 -o $PWD\publish\osx-x64 --self-contained false 
+dotnet publish $mcclcsproj -c Release -o $PWD\publish\local --self-contained false
 
-dotnet build $mcppcsproj -c Release
 dotnet publish $mcppcsproj -c Release -r linux-x64 -o $PWD\publish\linux-x64 --self-contained false
 dotnet publish $mcclcsproj -c Release -r win-x64 -o $PWD\publish\win-x64 --self-contained false 
 dotnet publish $mcclcsproj -c Release -r osx-x64 -o $PWD\publish\osx-x64 --self-contained false 
-
-Write-Host "version = $version" -ForegroundColor Green
 
 # Create win-x64 zip
 $releasedir = ".\release\win-x64"
@@ -78,28 +70,28 @@ Compress-Archive -Path $source -DestinationPath $archive
 $matlabfiles = "$PWD\matlab\post_processing\*"
 Compress-Archive -Path $matlabfiles -Update -DestinationPath $archive
 
-#Write-Host "Run MCCL MATLAB post-processing tests" -ForegroundColor Green
-## Change current dir to publish 
-#cd "$vtslevel\publish\$runtime"
+Write-Host "Run MCCL MATLAB post-processing tests" -ForegroundColor Green
+# Change current dir to publish\local to run tests with local executable
+cd "$vtslevel\publish\local"
 
-## Generate infiles and run Monte Carlo with general infile
-#./mc geninfiles
-#./mc infile=infile_one_layer_all_detectors.txt
+# Generate infiles and run Monte Carlo with general infile
+./mc.dll geninfiles
+./mc.dll infile=infile_one_layer_all_detectors.txt
 
-## Change current dir to MATLAB Monte Carlo post-processing
-#cd "$vtslevel\matlab\post_processing"
+# Change current dir to MATLAB Monte Carlo post-processing
+cd "$vtslevel\matlab\post_processing"
 
-## remove any residual folder
-## Copy results from Monte Carlo to current directory 
-#$MCmatlabdir = "$vtslevel\matlab\post_processing\one_layer_all_detectors"
-#Remove-Item  $MCmatlabdir -Recurse -ErrorAction Ignore
-#New-Item $MCmatlabdir -ItemType "directory"
-#$MCresults = "$vtslevel\publish\$runtime\one_layer_all_detectors\*"
-#Copy-Item -Path $MCresults -Destination $MCmatlabdir -Recurse -ErrorAction Ignore
+# remove any residual folder
+# Copy results from Monte Carlo to current directory 
+$MCmatlabdir = "$vtslevel\matlab\post_processing\one_layer_all_detectors"
+Remove-Item  $MCmatlabdir -Recurse -ErrorAction Ignore
+New-Item $MCmatlabdir -ItemType "directory"
+$MCresults = "$vtslevel\publish\local\one_layer_all_detectors\*"
+Copy-Item -Path $MCresults -Destination $MCmatlabdir -Recurse -ErrorAction Ignore
 
-## run load_results_script (default datanames is set to one_layer_all_detectors) 
-#matlab -wait -r "load_results_script; quit"
-## cd back to start
-#cd $vtslevel
+# run load_results_script (default datanames is set to one_layer_all_detectors) 
+matlab -wait -r "load_results_script; quit"
+# cd back to start
+cd $vtslevel
 
 Read-Host -Prompt "Press Enter to exit MCCL build process"
