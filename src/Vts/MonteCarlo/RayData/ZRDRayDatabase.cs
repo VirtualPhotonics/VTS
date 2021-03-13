b9d3309ed899b8e761d9004063536d6c9f9f700e
hayakawa <hayakawa@uci.edu>
2021-03-13 14:20:46 -0800

    MC: determined format of ZRD uncompressed file and was able to read in 1000 rays!

--- src/Vts/MonteCarlo/RayData/ZRDRayDatabase.cs
+++ src/Vts/MonteCarlo/RayData/ZRDRayDatabase.cs
@@ -1,8 +1,8 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
 using Vts.IO;
+using Vts.Common;
 
 namespace Vts.MonteCarlo.RayData
 {
@@ -19,15 +19,15 @@ namespace Vts.MonteCarlo.RayData
         public ZRDRayDatabase()
         {
         }
-        public static List<ZRDRayDataInUFD.ZRDRayDataPoint> ZRDRayDataPoints { get; private set; }
+        public static List<RayDataPoint> RayDataPoints { get; private set; }
         /// <summary>
         /// Static helper method to simplify reading from file
         /// </summary>
         /// <param name="fileName">The base filename for the database (no ".txt")</param>
         /// <returns>A new instance of SourceDatabase</returns>
-        public static List<ZRDRayDataInUFD.ZRDRayDataPoint> FromFile(string fileName)
+        public static List<RayDataPoint> FromFile(string fileName)
         {
-            ZRDRayDataPoints = new List<ZRDRayDataInUFD.ZRDRayDataPoint>();
+            RayDataPoints = new List<RayDataPoint>();
             // open stream
             using (Stream s = StreamFinder.GetFileStream(fileName, FileMode.Open))
             {
@@ -39,21 +39,28 @@ namespace Vts.MonteCarlo.RayData
                     int count = Marshal.SizeOf(typeof(ZRDRayDataInUFD.ZRDRayDataPoint));
                     for (int i = 0; i < 1000; i++)
                     {
-                        byte[] readBuffer = new byte[count];
                         int numSegments = br.ReadInt32();  // number of segments 
-                        var rayData = br.ReadBytes(count);
-                        GCHandle handle = GCHandle.Alloc(readBuffer, GCHandleType.Pinned);
-                        var zrdRayData = (ZRDRayDataInUFD.ZRDRayDataPoint)
-                            Marshal.PtrToStructure(handle.AddrOfPinnedObject(), 
-                            typeof(ZRDRayDataInUFD.ZRDRayDataPoint));
-                        ZRDRayDataPoints.Add(zrdRayData);
+                        var skipData = br.ReadBytes(56);
+                        double X = br.ReadDouble();
+                        double Y = br.ReadDouble();
+                        double Z = br.ReadDouble();
+                        double Ux = br.ReadDouble();
+                        double Uy = br.ReadDouble();
+                        double Uz = br.ReadDouble();
+                        skipData = br.ReadBytes(104); // skip to end of ZRDRayDataPoint
+                        skipData = br.ReadBytes((numSegments - 1) * count);                        
+                        //var rayData = br.ReadBytes(count);
+                        //byte[] readBuffer = new byte[count];
+                        //GCHandle handle = GCHandle.Alloc(readBuffer, GCHandleType.Pinned);
+                        //var zrdRayData = (ZRDRayDataInUFD.ZRDRayDataPoint)
+                        //    Marshal.PtrToStructure(handle.AddrOfPinnedObject(), 
+                        //    typeof(ZRDRayDataInUFD.ZRDRayDataPoint));
+                            RayDataPoints.Add(new RayDataPoint(
+                                new Position(X, Y, Z), new Direction(Ux, Uy, Uz), 1));
                     }
                 }
             }
-
-            return ZRDRayDataPoints;
+            return RayDataPoints;
         }
-
-
     }
 }
