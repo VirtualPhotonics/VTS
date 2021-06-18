6837c0fd6c35fd0d3a0dcd2d6b6b9840b7bc3950
Lisa Malenfant <lisa@silverliningcomputing.com>
2021-06-18 14:36:54 -0700

    Updated the version numbers for a release

--- BuildTestRelease.ps1
+++ BuildTestRelease.ps1
@@ -1,5 +1,5 @@
-$mccl_version = "5.1.0"
-$matlab_version = "4.11.0"
+$mccl_version = "6.0.0"
+$matlab_version = "5.0.0"
 
 Invoke-Expression ".\BuildTestReleaseMCCL.ps1 $mccl_version"
 Invoke-Expression ".\BuildTestReleaseMATLAB.ps1 $matlab_version"
