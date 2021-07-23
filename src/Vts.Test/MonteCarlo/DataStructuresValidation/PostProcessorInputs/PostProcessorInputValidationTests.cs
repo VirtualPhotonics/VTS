399e0f9ca13b2c20aabc2b1ea47074e89cf018b2
hayakawa <hayakawa@uci.edu>
2021-07-22 17:24:23 -0700

    Corrected bug in code in PostProcessorInputValidation code. The "if" statements in ValidatePhotonDatabaseExistence did not correctly test the TallyDetails settings.  First "if" was always being executed due to error and by chance worked because our current databases include DiffuseRelectanceDatabase.  However now adding code on a branch to create DiffuseTransmittanceDatabases which outed error. Added unit tests to verify that each of the "ifs" get executed correctly.

--- src/Vts.Test/MonteCarlo/DataStructuresValidation/PostProcessorInputs/PostProcessorInputValidationTests.cs
+++ src/Vts.Test/MonteCarlo/DataStructuresValidation/PostProcessorInputs/PostProcessorInputValidationTests.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using NUnit.Framework;
 using Vts.Common;
+using Vts.IO;
 using Vts.MonteCarlo;
 using Vts.MonteCarlo.Detectors;
 using Vts.MonteCarlo.Tissues;
@@ -10,45 +11,127 @@ namespace Vts.Test.MonteCarlo.DataStructuresValidation.PostProcessorInputs
     [TestFixture]
     public class PostProcessorInputValidationTests
     {
+        PostProcessorInput input;
+        /// <summary>
+        /// list of temporary files created by these unit tests
+        /// </summary>
+        List<string> listOfFolders = new List<string>()
+        {
+            "DiffuseReflectance", 
+        };
+        /// <summary>
+        /// clear all previously generated files.
+        /// </summary>
+        [OneTimeTearDown]
+        public void clear_folders_and_files()
+        {
+            // delete any previously generated files
+            foreach (var folder in listOfFolders)
+            {
+                FileIO.DeleteDirectory(folder);
+            }
+        }
+        [OneTimeSetUp]
+        public void setup_input()
+        {
+            {
+                var tissueInput = new MultiLayerTissueInput(
+                    new ITissueRegion[]
+                    {
+                        new LayerTissueRegion(
+                            new DoubleRange(double.NegativeInfinity, 0.0),
+                            new OpticalProperties(0.0, 1e-10, 1.0, 1.0)),
+                        new LayerTissueRegion(
+                            new DoubleRange(0.0, 100.0),
+                            new OpticalProperties(0.01, 1.0, 0.8, 1.4)), 
+                        new LayerTissueRegion(
+                            new DoubleRange(100.0, double.PositiveInfinity),
+                            new OpticalProperties(0.0, 1e-10, 1.0, 1.0))
+                    });
+                input = new PostProcessorInput(
+                    new List<IDetectorInput>()
+                    {
+                        new pMCROfRhoDetectorInput()
+                        {
+                            Rho = new DoubleRange(0.0, 100.0),
+                            // set perturbed ops to reference ops
+                            PerturbedOps = new List<OpticalProperties>()
+                            {
+                                tissueInput.Regions[0].RegionOP,
+                                tissueInput.Regions[1].RegionOP,
+                                tissueInput.Regions[2].RegionOP
+                            },
+                            PerturbedRegionsIndices = new List<int>() {1}
+                        }
+                    },
+                    "", "", ""
+                );
+            }
+        }
+
         /// <summary>
         /// Test to check that post-processor perturbed OPs are not negative
         /// </summary>
         [Test]
         public void validate_tissue_optical_properties_are_non_negative()
         {
-            var tissueInput = new MultiLayerTissueInput(
-                    new ITissueRegion[]
-                {
-                    new LayerTissueRegion(
-                        new DoubleRange(double.NegativeInfinity, 0.0),
-                        new OpticalProperties( 0.0, 1e-10, 1.0, 1.0)),
-                    new LayerTissueRegion(
-                        new DoubleRange(0.0, 100.0),
-                        new OpticalProperties(-1.0, 1.0, 0.8, 1.4)), // make mua negative
-                    new LayerTissueRegion(
-                        new DoubleRange(100.0, double.PositiveInfinity),
-                        new OpticalProperties(0.0, 1e-10, 1.0, 1.0))
-                });
-            var input = new PostProcessorInput(
-                new List<IDetectorInput>()
-                {
-                    new pMCROfRhoDetectorInput()
-                    {
-                        Rho=new DoubleRange(0.0, 100.0),
-                        // set perturbed ops to reference ops
-                        PerturbedOps = new List<OpticalProperties>() 
-                        { 
-                            tissueInput.Regions[0].RegionOP,
-                            tissueInput.Regions[1].RegionOP,
-                            tissueInput.Regions[2].RegionOP
-                        },
-                        PerturbedRegionsIndices = new List<int>() { 1 }
-                    }  
-                },
-                "","",""
-            );
+            // set pMC mua value to be negative
+            ((pMCROfRhoDetectorInput) input.DetectorInputs[0]).PerturbedOps[1].Mua = -0.01;
             var result = PostProcessorInputValidation.ValidateInput(input,"");
-            Assert.IsFalse(result.IsValid);
+            Assert.IsTrue(result.ValidationRule.Equals("Tissue optical properties mua, mus', n need to be non-negative"));
+            // set pMC mua value back to being positive so passes rest of tests
+            ((pMCROfRhoDetectorInput)input.DetectorInputs[0]).PerturbedOps[1].Mua = 0.01;
+        }
+        /// <summary>
+        /// Test to check input folder exists
+        /// </summary>
+        [Test]
+        public void validate_input_folder_existence()
+        {
+            var result = PostProcessorInputValidation.ValidateInput(input, "");
+            Assert.IsTrue(result.ValidationRule.Equals(
+                "PostProcessorInput: the input folder does not exist"));
+        }
+        /// <summary>
+        /// test that ValidatePhotonDatabaseExistence method returns correct ValidationRule
+        /// for different settings of TallyDetails.  To get to this method, validation
+        /// that the input folder exists must pass first so create folder.
+        /// Note: methods being tested are private so only way to test is through public
+        /// calling method.
+        /// Tests assume that the existence of the database file will fail and will pass
+        /// back a unique ValidationRule that can verify which "if" was used.
+        /// </summary>
+        [Test]
+        public void verify_database_aligns_with_TallyDetails_of_detector()
+        {
+            string folderName = "DiffuseReflectance";
+            // create folder
+            FileIO.CreateDirectory(folderName);
+            // test detector that is defined in the OneTimeSetup input = pMC reflectance tally
+            var result = PostProcessorInputValidation.ValidateInput(input, folderName);
+            Assert.IsTrue(result.ValidationRule.Equals(
+                "PostProcessorInput:  files DiffuseReflectanceDatabase or CollisionInfoDatabase do not exist"));
+            // clear out detector list
+            input.DetectorInputs.Clear();
+            // add reflectance tally detector
+            input.DetectorInputs.Add(new ROfRhoDetectorInput()); // this has IsReflectanceTally=true;
+            result = PostProcessorInputValidation.ValidateInput(input, folderName);
+            Assert.IsTrue(result.ValidationRule.Equals(
+                "PostProcessorInput:  file DiffuseReflectanceDatabase does not exist")); 
+            // clear out detector list
+            input.DetectorInputs.Clear();
+            // add transmittance tally detector
+            input.DetectorInputs.Add(new TOfRhoDetectorInput()); // this has IsTransmittanceTally=true;
+            result = PostProcessorInputValidation.ValidateInput(input, folderName);
+            Assert.IsTrue(result.ValidationRule.Equals(
+                "PostProcessorInput:  file DiffuseTransmittanceDatabase does not exist"));
+            // clear out detector list
+            input.DetectorInputs.Clear();
+            // add specular reflectance tally detector
+            input.DetectorInputs.Add(new RSpecularDetectorInput()); // this has IsSpecularReflectanceTally=true;
+            result = PostProcessorInputValidation.ValidateInput(input, folderName);
+            Assert.IsTrue(result.ValidationRule.Equals(
+                "PostProcessorInput:  file SpecularReflectanceDatabase does not exist"));
         }
     }
 }
