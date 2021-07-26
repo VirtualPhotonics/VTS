d292ee61d676afb8703f8cf8a0bad7b8ff53702e
hayakawa <hayakawa@uci.edu>
2021-07-26 16:59:12 -0700

    Wrote more tests for dMC detector validation and added new test for Enums.  Not sure if I can test the Properties in Enums.

--- src/Vts.Test/MonteCarlo/DataStructuresValidation/DetectorInputs/dMCdROfRhodMusDetectorInputValidationTests.cs
+++ src/Vts.Test/MonteCarlo/DataStructuresValidation/DetectorInputs/dMCdROfRhodMusDetectorInputValidationTests.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using Vts.Common;
 using Vts.MonteCarlo;
@@ -19,30 +20,44 @@ namespace Vts.Test.MonteCarlo.DataStructuresValidation.DetectorInputs
         public void validate_only_one_perturbed_region_index_specified()
         {
             var tissueInput = new MultiLayerTissueInput();
+            var detectorInput = new List<IDetectorInput>()
+            {
+                new dMCdROfRhodMusDetectorInput()
+                {
+                    Rho = new DoubleRange(0.0, 100.0),
+                    // set perturbed ops to reference ops
+                    PerturbedOps = new List<OpticalProperties>()
+                    {
+                        tissueInput.Regions[0].RegionOP,
+                        tissueInput.Regions[1].RegionOP,
+                        tissueInput.Regions[2].RegionOP
+                    },
+                    // set number of perturbed regions to 2
+                    PerturbedRegionsIndices = new List<int>() {1, 2}
+                }
+            };
             var input = new SimulationInput(
                 10,
                 "",
                 new SimulationOptions(),
                 new DirectionalPointSourceInput(),
                 tissueInput,
-                new List<IDetectorInput>()
-                {
-                    new dMCdROfRhodMusDetectorInput()
-                    {
-                        Rho=new DoubleRange(0.0, 100.0),
-                        // set perturbed ops to reference ops
-                        PerturbedOps = new List<OpticalProperties>() 
-                        { 
-                            tissueInput.Regions[0].RegionOP,
-                            tissueInput.Regions[1].RegionOP,
-                            tissueInput.Regions[2].RegionOP
-                        },
-                        PerturbedRegionsIndices = new List<int>() { 1, 2 }
-                    }
-                }
+                detectorInput
             );
             var result = SimulationInputValidation.ValidateInput(input);
             Assert.IsFalse(result.IsValid);
+            // redefine detector number of perturbed region to pass validation
+            ((dMCdROfRhodMusDetectorInput)detectorInput.First()).PerturbedRegionsIndices = new List<int> { 1 };
+            input = new SimulationInput(
+                10,
+                "",
+                new SimulationOptions(),
+                new DirectionalPointSourceInput(),
+                tissueInput,
+                detectorInput
+            );
+            result = SimulationInputValidation.ValidateInput(input);
+            Assert.IsTrue(result.IsValid);
         }
     }
 }
