70da15e9d2bf0c6ec4d5414062517d645337e7f2
hayakawa <hayakawa@uci.edu>
2021-07-29 22:12:30 -0700

    Broke out Point and Line Source composite test into separate class tests.  Created ValidationData class for each to have data in central place.

---
+++ src/Vts.Test/MonteCarlo/Sources/PointAndLineSources/LineSources/CustomLineSourceTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using Vts.Common;
+using Vts.MonteCarlo;
+using Vts.MonteCarlo.Helpers;
+using Vts.MonteCarlo.Sources;
+using Vts.MonteCarlo.Sources.SourceProfiles;
+using Vts.MonteCarlo.Tissues;
+
+namespace Vts.Test.MonteCarlo.Sources
+{
+    /// <summary>
+    /// Unit tests for Custom Line Sources
+    /// </summary>
+    [TestFixture]
+    public class CustomLineSourceTests
+    {
+        private static LineSourcesValidationData _validationData;
+
+        [OneTimeSetUp]
+        public void setup_validation_data()
+        {
+            if (_validationData == null)
+            {
+                _validationData = new LineSourcesValidationData();
+                _validationData.ReadData();
+            }
+        }
+
+        /// <summary>
+        /// Validate General Constructor of Custom Flat Line Source
+        /// </summary>
+        [Test]
+        public void validate_general_constructor_with_flat_profiletype_for_custom_line_source_test()
+        {
+            Random rng = new MathNet.Numerics.Random.MersenneTwister(0); // not really necessary here, as this is now the default
+            ITissue tissue = new MultiLayerTissue();             
+            var profile = new FlatSourceProfile();
+
+            var ps = new CustomLineSource(
+                _validationData.LengthX, 
+                profile, 
+                _validationData.PolRange, 
+                _validationData.AziRange, 
+                _validationData.Direction, 
+                _validationData.Translation, 
+                _validationData.AngPair)
+            {
+                Rng = rng
+            };
+
+            var photon = ps.GetNextPhoton(tissue);
+
+            Assert.Less(Math.Abs(photon.DP.Direction.Ux - _validationData.Tp[18]), _validationData.AcceptablePrecision);
+            Assert.Less(Math.Abs(photon.DP.Direction.Uy - _validationData.Tp[19]), _validationData.AcceptablePrecision);
+            Assert.Less(Math.Abs(photon.DP.Direction.Uz - _validationData.Tp[20]), _validationData.AcceptablePrecision);
+
+            Assert.Less(Math.Abs(photon.DP.Position.X - _validationData.Tp[21]), _validationData.AcceptablePrecision);
+            Assert.Less(Math.Abs(photon.DP.Position.Y - _validationData.Tp[22]), _validationData.AcceptablePrecision);
+            Assert.Less(Math.Abs(photon.DP.Position.Z - _validationData.Tp[23]), _validationData.AcceptablePrecision);
+        }
+
+
+        /// <summary>
+        /// Validate General Constructor of Custom Gaussian Line Source
+        /// </summary>
+        [Test]
+        public void validate_general_constructor_with_gaussian_profiletype_for_custom_line_source_test()
+        {         
+
+            Random rng = new MathNet.Numerics.Random.MersenneTwister(0); // not really necessary here, as this is now the default
+            ITissue tissue = new MultiLayerTissue();
+            var profile = new GaussianSourceProfile(_validationData.BdFWHM);
+
+            var ps = new CustomLineSource(
+                _validationData.LengthX, 
+                profile, 
+                _validationData.PolRange, 
+                _validationData.AziRange, 
+                _validationData.Direction, 
+                _validationData.Translation, 
+                _validationData.AngPair)
+            {
+                Rng = rng
+            };
+
+            var photon = ps.GetNextPhoton(tissue);
+
+            Assert.Less(Math.Abs(photon.DP.Direction.Ux - _validationData.Tp[24]), _validationData.AcceptablePrecision);
+            Assert.Less(Math.Abs(photon.DP.Direction.Uy - _validationData.Tp[25]), _validationData.AcceptablePrecision);
+            Assert.Less(Math.Abs(photon.DP.Direction.Uz - _validationData.Tp[26]), _validationData.AcceptablePrecision);
+
+            Assert.Less(Math.Abs(photon.DP.Position.X - _validationData.Tp[27]), _validationData.AcceptablePrecision);
+            Assert.Less(Math.Abs(photon.DP.Position.Y - _validationData.Tp[28]), _validationData.AcceptablePrecision);
+            Assert.Less(Math.Abs(photon.DP.Position.Z - _validationData.Tp[29]), _validationData.AcceptablePrecision);
+        }
+
+    }
+}
---
+++ src/Vts.Test/MonteCarlo/Sources/PointAndLineSources/PointSources/DirectionalPointSourceTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using Vts.Common;
+using Vts.MonteCarlo;
+using Vts.MonteCarlo.Helpers;
+using Vts.MonteCarlo.Sources;
+using Vts.MonteCarlo.Tissues;
+
+namespace Vts.Test.MonteCarlo.Sources
+{
+    /// <summary>
+    /// Unit tests for Directional Point Sources
+    /// </summary>
+    [TestFixture]
+    public class DirectionalPointSourceTests
+    {
+        private static PointSourcesValidationData _validationData;
+
+        [OneTimeSetUp]
+        public void setup_validation_data()
+        {
+            if (_validationData == null)
+            {
+                _validationData = new PointSourcesValidationData();
+                _validationData.ReadData();
+            }
+        }
+
+        /// <summary>
+        /// Validate General Constructor of Directional Point Source
+        /// </summary>
+        [Test]
+        public void validate_general_constructor_with_flat_profiletype_for_directional_point_source_test()
+        {
+            Random rng = new MathNet.Numerics.Random.MersenneTwister(0); // not really necessary here, as this is now the default
+            ITissue tissue = new MultiLayerTissue(); // todo: remove           
+
+            var ps = new DirectionalPointSource(_validationData.Direction, _validationData.Translation)
+            {
+                Rng = rng
+            };
+
+            var photon = ps.GetNextPhoton(tissue);
+
+            Assert.Less(Math.Abs(photon.DP.Direction.Ux - _validationData.Tp[22]), _validationData.AcceptablePrecision);
+            Assert.Less(Math.Abs(photon.DP.Direction.Uy - _validationData.Tp[23]), _validationData.AcceptablePrecision);
+            Assert.Less(Math.Abs(photon.DP.Direction.Uz - _validationData.Tp[24]), _validationData.AcceptablePrecision);
+
+            Assert.Less(Math.Abs(photon.DP.Position.X - _validationData.Tp[25]), _validationData.AcceptablePrecision);
+            Assert.Less(Math.Abs(photon.DP.Position.Y - _validationData.Tp[26]), _validationData.AcceptablePrecision);
+            Assert.Less(Math.Abs(photon.DP.Position.Z - _validationData.Tp[27]), _validationData.AcceptablePrecision);    
+        }
+    }
+}
