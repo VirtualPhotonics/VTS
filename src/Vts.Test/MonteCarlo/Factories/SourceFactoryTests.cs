4f9714c22dd30b36104e35a0ef107093690080c6
hayakawa <hayakawa@uci.edu>
2021-07-31 18:48:50 -0700

    Started adding Factory tests.  Tried to write a Mock but need Lisa's help.

---
+++ src/Vts.Test/MonteCarlo/Factories/SourceFactoryTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Vts.Common;
+using Vts.MonteCarlo;
+using Vts.MonteCarlo.Sources;
+using Vts.MonteCarlo.Factories;
+using MathNet.Numerics.Random;
+using Vts.IO;
+
+namespace Vts.Test.MonteCarlo.Factories
+{
+    /// <summary>
+    /// Unit tests for SourceFactory
+    /// </summary>
+    [TestFixture]
+    public class SourceFactoryTests
+    {
+        /// <summary>
+        /// Simulate basic usage of SourceFactory
+        /// </summary>
+        [Test]
+        public void Demonstrate_GetSource()
+        {
+            ISourceInput sourceInput = new DirectionalPointSourceInput
+                {
+                    SourceType =  "DirectionalPoint",
+                    PointLocation = new Position(0, 0, 0),
+                    Direction = new Direction(0, 0, 1),
+                    InitialTissueRegionIndex = 0
+                };
+            var source = SourceFactory.GetSource(sourceInput, new MersenneTwister(0));
+
+            Assert.NotNull(source);
+        }
+    }
+}
---
+++ src/Vts.Test/MonteCarlo/Factories/TissueFactoryTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NUnit.Framework;
+using Vts.MonteCarlo;
+using Vts.MonteCarlo.Tissues;
+using Vts.MonteCarlo.Factories;
+using Moq;
+
+namespace Vts.Test.MonteCarlo.Factories
+{
+    /// <summary>
+    /// Unit tests for TissueFactory
+    /// </summary>
+    [TestFixture]
+    public class TissueFactoryTests
+    {
+        /// <summary>
+        /// Simulate basic usage of TissueFactory
+        /// </summary>
+        [Test]
+        public void Demonstrate_GetTissue()
+        {
+            ITissueInput tissueInput = new MultiLayerTissueInput
+                {
+                    TissueType =  "MultiLayer",
+                };
+            var tissue = TissueFactory.GetTissue(
+                tissueInput,
+                AbsorptionWeightingType.Analog,
+                PhaseFunctionType.HenyeyGreenstein,
+                0.0);
+
+            Assert.NotNull(tissue);
+        }        
+        /// <summary>
+        /// Simulate erroneous invocation
+        /// </summary>
+        [Ignore("need to ask Lisa for help")]
+        [Test]
+        public void Demonstrate_GetTissue_returns_null_on_faulty_tissue_input()
+        {
+            var tissueInputMock = new Mock<MultiLayerTissueInput>();
+            tissueInputMock.Setup(x => x.CreateTissue(
+                AbsorptionWeightingType.Analog,
+                PhaseFunctionType.Bidirectional,
+                0.0)).Returns((ITissue)null);
+
+            var tissue = TissueFactory.GetTissue(
+                tissueInputMock.Object,
+            AbsorptionWeightingType.Analog,
+            PhaseFunctionType.Bidirectional,
+            0.0);
+
+            Assert.IsNull(tissue);
+        }
+    }
+}
