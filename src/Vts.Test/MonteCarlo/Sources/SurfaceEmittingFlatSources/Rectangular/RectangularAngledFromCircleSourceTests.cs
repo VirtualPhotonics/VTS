ad0788c7c20b60c0c19f5cd7a68d7660ed7ee6be
hayakawa <hayakawa@uci.edu>
2021-08-09 15:39:09 -0700

    Added more testing to recently expanded sources.

--- src/Vts.Test/MonteCarlo/Sources/SurfaceEmittingFlatSources/Rectangular/RectangularAngledFromCircleSourceTests.cs
+++ src/Vts.Test/MonteCarlo/Sources/SurfaceEmittingFlatSources/Rectangular/RectangularAngledFromCircleSourceTests.cs
@@ -48,21 +48,23 @@ namespace Vts.Test.MonteCarlo.Sources
         [Test]
         public void validate_RectangularAngledFromCircle_source()
         {
-            Random rng = new MathNet.Numerics.Random.MersenneTwister(0); // not really necessary here, as this is now the default
+            Random rng =
+                new MathNet.Numerics.Random.MersenneTwister(0); // not really necessary here, as this is now the default
             ITissue tissue = new MultiLayerTissue();
             var profile = new FlatSourceProfile();
             var rectLengthX = 10.0;
             var rectWidthY = 5.0;
+            // centered rectangle
             var translationFromOrigin = new Position(0, 0, 0);
             var radiusInAir = 0.1;
             var circleInAirTranslationFromOrigin = new Position(0, 0, -10);
 
             var ps = new RectangularAngledFromCircleSource(
-                rectLengthX, 
+                rectLengthX,
                 rectWidthY,
                 profile,
                 translationFromOrigin,
-                radiusInAir, 
+                radiusInAir,
                 circleInAirTranslationFromOrigin,
                 0)
             {
@@ -72,10 +74,33 @@ namespace Vts.Test.MonteCarlo.Sources
             // make sure initial position is at tissue surface
             Assert.AreEqual(photon.DP.Position.Z, 0.0);
             // make sure initial position is inside rectangle
-            Assert.IsTrue((photon.DP.Position.X < 5)&&(photon.DP.Position.X > -5));
+            Assert.IsTrue((photon.DP.Position.X < 5) && (photon.DP.Position.X > -5));
             Assert.IsTrue((photon.DP.Position.Y < 2.5) && (photon.DP.Position.Y > -2.5));
             // make sure angle is less than 45 degrees
             Assert.IsTrue(photon.DP.Direction.Uz >= 1 / Math.Sqrt(2));
+
+            // test off center rectangle
+            translationFromOrigin = new Position(1.0, 0, 0);
+            ps = new RectangularAngledFromCircleSource(
+                rectLengthX,
+                rectWidthY,
+                profile,
+                translationFromOrigin,
+                radiusInAir,
+                circleInAirTranslationFromOrigin,
+                0)
+            {
+                Rng = rng
+            };
+            for (int i = 0; i < 10; i++) // test 10 photons
+            {
+                photon = ps.GetNextPhoton(tissue);
+                // make sure initial position is at tissue surface
+                Assert.AreEqual(photon.DP.Position.Z, 0.0);
+                // make sure initial position is inside rectangle
+                Assert.IsTrue((photon.DP.Position.X < 5 + 1) && (photon.DP.Position.X > -5 + 1));
+                Assert.IsTrue((photon.DP.Position.Y < 2.5) && (photon.DP.Position.Y > -2.5));
+            }
         }
     }
 }
