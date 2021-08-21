76df4982b23eeadb159d9ad537d8c4413e60c733
Lisa Malenfant <lisa.malenfant@uci.edu>
2021-08-20 19:19:57 -0700

    Added unit tests for BindableObject, BindableObjectWithChangeTracking and PropertyDependencyManager

---
+++ src/Vts.Test/Common/BindableObjectTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Vts.Test.Common
+{
+    [TestFixture]
+    class BindableObjectTests
+    {
+        [Test]
+        public void Test_VerifyProperty_throws_exception()
+        {
+            var myTestClass = new MyTestClass { First = 0 };
+            Assert.Throws<Exception>(() => myTestClass.OnPropertyChanged("NotAProperty"));
+        }
+
+        [Test]
+        public void Test_GetPropertyChangedEventArgs_throws_exception()
+        {
+            var myTestClass = new MyTestClass { First = 0 };
+            Assert.Throws<ArgumentException>(() => BindableObject.GetPropertyChangedEventArgs(""));
+        }
+    }
+}
---
+++ src/Vts.Test/Common/BindableObjectWithChangeTrackingTests.cs
@@ -0,0 +1,89 @@
+﻿using System.ComponentModel;
+using NUnit.Framework;
+
+namespace Vts.Test.Common
+{
+    public class MyTestClass : BindableObjectWithChangeTracking
+    {
+        private int _first;
+        private int _third;
+
+        public int First
+        {
+            get => _first;
+            set
+            {
+                HandleChangeTracking("First", ref _first, ref _first);
+                SetProperty("First", ref _first, ref value);
+                _first = value;
+            }
+        }
+
+        public int Second { get; set; }
+
+        [DependsOn("Second")]
+        public int Third
+        {
+            get => _third;
+            set
+            {
+                _third = value;
+                OnPropertyChanged("Second");
+            }
+        }
+
+        public bool PropertyWasChanged { get; set; }
+
+        public MyTestClass()
+        {
+            PropertyChanged += myTestClass_PropertyChanged;
+        }
+
+        private void myTestClass_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            PropertyWasChanged = true;
+        }
+    }
+
+    [TestFixture]
+    internal class BindableObjectWithChangeTrackingTests
+    {
+        [Test]
+        public void Test_SetProperty()
+        {
+            var myTestClass = new MyTestClass { First = 6 };
+            Assert.IsTrue(myTestClass.SettingsChanged);
+        }
+
+        [Test]
+        public void Test_HandleChangeTracking_changed()
+        {
+            var myTestClass = new MyTestClass { First = 6 };
+            myTestClass.Reset();
+            Assert.IsFalse(myTestClass.SettingsChanged);
+            myTestClass.First = 0;
+            Assert.AreEqual(0, myTestClass.First);
+            Assert.IsTrue(myTestClass.SettingsChanged);
+            myTestClass.First = 2;
+            Assert.IsTrue(myTestClass.SettingsChanged);
+        }
+
+        [Test]
+        public void Test_HandleChangeTracking_unchanged()
+        {
+            var myTestClass = new MyTestClass { First = 0 };
+            Assert.AreEqual(0, myTestClass.First);
+            Assert.IsTrue(myTestClass.SettingsChanged);
+            myTestClass.First = 0;
+            Assert.IsFalse(myTestClass.SettingsChanged);
+        }
+
+        [Test]
+        public void Test_Reset()
+        {
+            var myTestClass = new MyTestClass { First = 0 };
+            myTestClass.Reset();
+            Assert.IsFalse(myTestClass.SettingsChanged);
+        }
+    }
+}
