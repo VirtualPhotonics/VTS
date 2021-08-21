b9bca4e147fd6b14ac8836e7a155d4913f1f9c8f
Lisa Malenfant <lisa.malenfant@uci.edu>
2021-08-20 20:41:49 -0700

    Added unit tests for KnownTypes, LibraryIO and RangeOfT

---
+++ src/Vts.Test/IO/LibraryIOTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Framework;
+using Vts.IO;
+
+namespace Vts.Test.IO
+{
+    [TestFixture]
+    internal class LibraryIOTests
+    {
+        [Test]
+        public void Test_EnsureDllIsLoaded()
+        {
+            var assembliesBefore = AppDomain.CurrentDomain.GetAssemblies();
+            var assemblyCount = assembliesBefore.Length;
+            LibraryIO.EnsureDllIsLoaded("Vts.dll");
+            var assembliesAfter = AppDomain.CurrentDomain.GetAssemblies();
+            Assert.Greater(assembliesAfter.Length, assemblyCount);
+        }
+    }
+}
