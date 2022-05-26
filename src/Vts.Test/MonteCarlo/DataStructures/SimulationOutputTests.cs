508c1a9de409f353cf4585e853bb600fba6d407d
hayakawa <hayakawa@uci.edu>
2022-05-25 19:57:10 -0700

    Removed code from SimulationOutput that checks for duplicate Name and if so, concats number to Name and adds to dictionary.  Instead added code to SimulationInputValidation to check if detector input has duplicate names and flags user to revise infile to have unique detector names.  Removed unit test in SimulationOutputTests that verified removed code.  Added unit test to SimulationInputValidation to check that duplicate name error creates invalid input. Fixed unit test, DetectorNATests, that had duplicate detector but shouldn't have because of new code!

--- src/Vts.Test/MonteCarlo/DataStructures/SimulationOutputTests.cs
+++ src/Vts.Test/MonteCarlo/DataStructures/SimulationOutputTests.cs
@@ -34,31 +34,6 @@ namespace Vts.Test.MonteCarlo
             Assert.AreEqual(Math.PI, angle.Stop);
             Assert.AreEqual(10, angle.Count);
         }
-        /// <summary>
-        /// Test to check that addition of "1" to detector name is successful when
-        /// two detectors with same name are added to ResultsDictionary
-        /// </summary>
-        [Test]
-        public void validate_whether_two_detectors_with_same_name_are_added_to_ResultsDictionary_correctly()
-        {
-            var detectorList =
-                new List<IDetector> 
-                {
-                    new ROfRhoDetector() { Rho = new DoubleRange(0, 10, 10), TallySecondMoment = false, Name = "testName"},
-                    new ROfRhoDetector() { Rho = new DoubleRange(0, 20, 20), TallySecondMoment = false, Name = "testName"}
-                };
-            SimulationOutput output = new SimulationOutput(new SimulationInput(), detectorList);
-            var detector = (ROfRhoDetector)output.ResultsDictionary["testName"];
-            var rho = detector.Rho;
-            Assert.AreEqual(0d, rho.Start);
-            Assert.AreEqual(10, rho.Stop);
-            Assert.AreEqual(10, rho.Count);
-            var detector1 = (ROfRhoDetector)output.ResultsDictionary["testName1"];
-            var rho1 = detector1.Rho;
-            Assert.AreEqual(0d, rho1.Start);
-            Assert.AreEqual(20, rho1.Stop);
-            Assert.AreEqual(20, rho1.Count);
-        }
 
     }
 }
