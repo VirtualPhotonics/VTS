d8b1ec27492cba81ac2870b7df80f3d468432a71
hayakawa <hayakawa@uci.edu>
2022-07-25 15:48:51 -0700

    Added parameter and exception description

--- src/Vts/MonteCarlo/Sources/VolumetricSources/FluorescenceEmission/AOfRhoAndZLoader.cs
+++ src/Vts/MonteCarlo/Sources/VolumetricSources/FluorescenceEmission/AOfRhoAndZLoader.cs
@@ -59,6 +59,7 @@ namespace Vts.MonteCarlo.Sources
         /// <param name="inputFolder">input folder where excitation results reside</param>
         /// <param name="infile">simulation infile of excitation simulation</param>
         /// <param name="fluorescentTissueRegionIndex">integer index of tissue region where fluorescence is</param>
+        /// <exception cref="ArgumentException">throws ArgumentException if infile is not there</exception>
         public AOfRhoAndZLoader(string inputFolder, string infile, int fluorescentTissueRegionIndex)
         {
             if (infile != "")
