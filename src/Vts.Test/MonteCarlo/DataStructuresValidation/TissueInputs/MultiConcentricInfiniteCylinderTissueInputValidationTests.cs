f8b2e24d9a614a9afb1f8831e631b0fb969c4ab7
hayakawa <hayakawa@uci.edu>
2022-09-29 14:26:35 -0700

    Rename class file to match class and cleaned up tests for easier readability.

--- src/Vts.Test/MonteCarlo/DataStructuresValidation/TissueInputs/MultiConcentricInfiniteCylinderTissueInputValidationTests.cs
+++ src/Vts.Test/MonteCarlo/DataStructuresValidation/TissueInputs/MultiConcentricInfiniteCylinderTissueInputValidationTests.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using Vts.Common;
 using Vts.MonteCarlo;
+using Vts.MonteCarlo.Detectors;
 using Vts.MonteCarlo.Sources;
 using Vts.MonteCarlo.Tissues;
 
@@ -47,7 +48,10 @@ namespace Vts.Test.MonteCarlo.DataStructuresValidation.TissueInputs
                             new OpticalProperties(0.0, 1e-10, 1.0, 1.0))
                     }
                 ),
-                new List<IDetectorInput>() { }
+                new List<IDetectorInput>() 
+                {
+                    new FluenceOfXAndYAndZDetectorInput()
+                }
             );
             var result = SimulationInputValidation.ValidateInput(input);
             Assert.IsFalse(result.IsValid);
@@ -68,9 +72,13 @@ namespace Vts.Test.MonteCarlo.DataStructuresValidation.TissueInputs
                     {
                         // set one infinite cylinder radius to 0.0
                         new InfiniteCylinderTissueRegion(
-                            new Position(0, 0, 1), 0.0, new OpticalProperties()),
-                                                new InfiniteCylinderTissueRegion(
-                            new Position(0, 0, 1), 1.0, new OpticalProperties()),
+                            new Position(0, 0, 1), 
+                            0.0, 
+                            new OpticalProperties()),
+                        new InfiniteCylinderTissueRegion(
+                            new Position(0, 0, 1), 
+                            1.0, 
+                            new OpticalProperties()),
                     },
                     new ITissueRegion[]
                     { 
@@ -85,7 +93,10 @@ namespace Vts.Test.MonteCarlo.DataStructuresValidation.TissueInputs
                             new OpticalProperties(0.0, 1e-10, 1.0, 1.0))
                     }
                 ),
-                new List<IDetectorInput>(){ }
+                new List<IDetectorInput>()
+                {
+                    new FluenceOfXAndYAndZDetectorInput()
+                }
             );
             var result = SimulationInputValidation.ValidateInput(input);
             Assert.IsFalse(result.IsValid);
@@ -105,9 +116,13 @@ namespace Vts.Test.MonteCarlo.DataStructuresValidation.TissueInputs
                     new InfiniteCylinderTissueRegion[]
                     {
                         new InfiniteCylinderTissueRegion(
-                            new Position(0, 0, 1), 2.0, new OpticalProperties()),
-                                                new InfiniteCylinderTissueRegion(
-                            new Position(0, 0, 1), 1.0, new OpticalProperties()),
+                            new Position(0, 0, 1), 
+                            2.0, 
+                            new OpticalProperties()),
+                        new InfiniteCylinderTissueRegion(
+                            new Position(0, 0, 1), 
+                            1.0, 
+                            new OpticalProperties()),
                     },
                     new ITissueRegion[]
                     { 
@@ -119,7 +134,10 @@ namespace Vts.Test.MonteCarlo.DataStructuresValidation.TissueInputs
                             new OpticalProperties(0.0, 1e-10, 1.0, 1.0))
                     }
                 ), 
-                new List<IDetectorInput>(){ }
+                new List<IDetectorInput>()
+                {
+                    new FluenceOfXAndYAndZDetectorInput()
+                }
             );
             var result = SimulationInputValidation.ValidateInput(input);
             Assert.IsFalse(result.IsValid);
@@ -140,9 +158,13 @@ namespace Vts.Test.MonteCarlo.DataStructuresValidation.TissueInputs
                     {
                         // set different Centers
                         new InfiniteCylinderTissueRegion(
-                            new Position(0, 0, 5), 2.0, new OpticalProperties()),
-                                                new InfiniteCylinderTissueRegion(
-                            new Position(0, 0, 1), 1.0, new OpticalProperties()),
+                            new Position(0, 0, 5), 
+                            2.0, 
+                            new OpticalProperties()),
+                        new InfiniteCylinderTissueRegion(
+                            new Position(0, 0, 1), 
+                            1.0,
+                            new OpticalProperties()),
                     },
                     new ITissueRegion[]
                     {
@@ -157,16 +179,19 @@ namespace Vts.Test.MonteCarlo.DataStructuresValidation.TissueInputs
                             new OpticalProperties(0.0, 1e-10, 1.0, 1.0))
                     }
                 ), 
-                new List<IDetectorInput>() { }
+                new List<IDetectorInput>() 
+                {
+                    new FluenceOfXAndYAndZDetectorInput()
+                }
             );
             var result = SimulationInputValidation.ValidateInput(input);
             Assert.IsFalse(result.IsValid);
         }
         /// <summary>
-        /// Test to check that infinite cylinder is entirely contained within tissue layer
+        /// Test to check that infinite cylinders are entirely contained within tissue layer
         /// </summary>
         [Test]
-        public void validate_ellipsoid_is_within_tissue_layer()
+        public void validate_infinite_cylinders_are_within_tissue_layer()
         {
             var input = new SimulationInput(
                 10,
@@ -176,11 +201,15 @@ namespace Vts.Test.MonteCarlo.DataStructuresValidation.TissueInputs
                 new MultiConcentricInfiniteCylinderTissueInput(
                     new InfiniteCylinderTissueRegion[]
                     {
-                        // set one infinite cylinder radius to 0.0
+                        // set one infinite cylinder radius to go beyond layer
                         new InfiniteCylinderTissueRegion(
-                            new Position(0, 0, 1), 15.0, new OpticalProperties()),
+                            new Position(0, 0, 1), 
+                            15.0, 
+                            new OpticalProperties()),
                         new InfiniteCylinderTissueRegion(
-                            new Position(0, 0, 1), 1.0, new OpticalProperties()),
+                            new Position(0, 0, 1), 
+                            1.0, 
+                            new OpticalProperties()),
                     }, new ITissueRegion[]
                     { 
                         new LayerTissueRegion(
@@ -194,16 +223,19 @@ namespace Vts.Test.MonteCarlo.DataStructuresValidation.TissueInputs
                             new OpticalProperties(0.0, 1e-10, 1.0, 1.0))
                     }
                 ), 
-                new List<IDetectorInput>() { }
+                new List<IDetectorInput>() 
+                {
+                    new FluenceOfXAndYAndZDetectorInput() 
+                }
             );
             var result = SimulationInputValidation.ValidateInput(input);
             Assert.IsFalse(result.IsValid);
         }
         /// <summary>
-        /// Test to check that ellipsoid refractive index matches refractive index of surrounding layer
+        /// Test to check that infinite cylinders refractive index matches refractive index of surrounding layer
         /// </summary>
         [Test]
-        public void validate_ellipsoid_refractive_index_matches_that_of_surrounding_layer()
+        public void validate_infinite_cylinders_refractive_index_matches_that_of_surrounding_layer()
         {
             var input = new SimulationInput(
                 10,
@@ -215,10 +247,13 @@ namespace Vts.Test.MonteCarlo.DataStructuresValidation.TissueInputs
                     {
                         // set refractive index to not match
                         new InfiniteCylinderTissueRegion(
-                            new Position(0, 0, 1), 0.0, 
+                            new Position(0, 0, 1), 
+                            2.0, 
                             new OpticalProperties(1.0, 1.0, 0.8, 1.6)),
                         new InfiniteCylinderTissueRegion(
-                            new Position(0, 0, 1), 1.0, new OpticalProperties()),
+                            new Position(0, 0, 1), 
+                            1.0,
+                            new OpticalProperties(1.0, 1.0, 0.8, 1.4)),
                     },
                     new ITissueRegion[]
                     { 
@@ -233,10 +268,60 @@ namespace Vts.Test.MonteCarlo.DataStructuresValidation.TissueInputs
                             new OpticalProperties(0.0, 1e-10, 1.0, 1.0))
                     }
                 ),
-                new List<IDetectorInput>() { }
+                new List<IDetectorInput>() 
+                { 
+                    new FluenceOfXAndYAndZDetectorInput() 
+                }
             );
             var result = SimulationInputValidation.ValidateInput(input);
             Assert.IsFalse(result.IsValid);
+        }
+        /// <summary>
+        /// Test to check two layer tissue with one layer enclosing concentric infinite cylinders works
+        /// </summary>
+        [Test]
+        public void validate_infinite_cylinders_within_one_layer_of_multilayer_works()
+        {
+            var input = new SimulationInput(
+                10,
+                "",
+                new SimulationOptions(),
+                new DirectionalPointSourceInput(),
+                new MultiConcentricInfiniteCylinderTissueInput(
+                    new InfiniteCylinderTissueRegion[]
+                    {
+                        new InfiniteCylinderTissueRegion(
+                            new Position(0, 0, 5), 
+                            2.0,
+                            new OpticalProperties(1.0, 1.0, 0.8, 1.4)),
+                        new InfiniteCylinderTissueRegion(
+                            new Position(0, 0, 5), 
+                            1.0, 
+                            new OpticalProperties(0.01, 1.5, 0.9, 1.4)),
+                    },
+                    new ITissueRegion[]
+                    {
+                        new LayerTissueRegion(
+                            new DoubleRange(double.NegativeInfinity, 0.0),
+                            new OpticalProperties(0.0, 1e-10, 1.0, 1.0)),
+                        new LayerTissueRegion(
+                            new DoubleRange(0.0, 10.0),
+                            new OpticalProperties(0.01, 1.0, 0.8, 1.4)),
+                        new LayerTissueRegion(
+                            new DoubleRange(10.0, 20.0),
+                            new OpticalProperties(0.1, 1.5, 0.8, 1.3)),
+                        new LayerTissueRegion(
+                            new DoubleRange(20.0, double.PositiveInfinity),
+                            new OpticalProperties(0.0, 1e-10, 1.0, 1.0))
+                    }
+                ),
+                new List<IDetectorInput>() 
+                { 
+                    new FluenceOfXAndYAndZDetectorInput()
+                }
+            );
+            var result = SimulationInputValidation.ValidateInput(input);
+            Assert.IsTrue(result.IsValid);
         }
     }
 }
