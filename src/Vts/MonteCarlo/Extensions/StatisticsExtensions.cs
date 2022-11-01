038e218e86c04930c5c6081872c81e614fc4c4a5
hayakawa <hayakawa@uci.edu>
2022-11-01 12:42:35 -0700

    Modifications to fix bug that counted number of photons killed by Russian Roulette incorrectly.

--- src/Vts/MonteCarlo/Extensions/StatisticsExtensions.cs
+++ src/Vts/MonteCarlo/Extensions/StatisticsExtensions.cs
@@ -8,41 +8,46 @@ namespace Vts.MonteCarlo.Extensions
     public static class StatisticsExtensions
     {
         /// <summary>
-        /// Method to determine statistics about how the photon died.
+        /// Method to determine statistics about how the photon died.  These states
+        /// are typically mutually exclusive, however there are occurrences of photons
+        /// that are killed by RR at start of track that crosses boundary
+        /// and gets that state set as well but does not tally.
+        /// It is for this reason the check for RR is first.
         /// </summary>
         /// <param name="statistics">SimulationStatistics class where statistics are kept</param>
         /// <param name="dp">PhotonDataPoint</param>
         /// <returns>Updated SimulationStatistics</returns>
         public static SimulationStatistics TrackDeathStatistics(this SimulationStatistics statistics, PhotonDataPoint dp)
         {
-            if (dp.StateFlag.HasFlag(PhotonStateType.PseudoReflectedTissueBoundary))
-            {
-                ++statistics.NumberOfPhotonsOutTopOfTissue;
-            }
-            if (dp.StateFlag.HasFlag(PhotonStateType.PseudoTransmittedTissueBoundary))
-            {
-                ++statistics.NumberOfPhotonsOutBottomOfTissue;
-            }
-            if (dp.StateFlag.HasFlag(PhotonStateType.Absorbed))
-            {
-                ++statistics.NumberOfPhotonsAbsorbed;
-            }
-            if (dp.StateFlag.HasFlag(PhotonStateType.PseudoSpecularTissueBoundary))
-            {
-                ++statistics.NumberOfPhotonsSpecularReflected;
-            }
-            if (dp.StateFlag.HasFlag(PhotonStateType.KilledOverMaximumCollisions))
-            {
-                ++statistics.NumberOfPhotonsKilledOverMaximumCollisions;
-            }
-            if (dp.StateFlag.HasFlag(PhotonStateType.KilledOverMaximumPathLength))
-            {
-                ++statistics.NumberOfPhotonsKilledOverMaximumPathLength;
-            }
             if (dp.StateFlag.HasFlag(PhotonStateType.KilledRussianRoulette))
             {
                 ++statistics.NumberOfPhotonsKilledByRussianRoulette;
             }
+            else if (dp.StateFlag.HasFlag(PhotonStateType.PseudoReflectedTissueBoundary))
+            {
+                ++statistics.NumberOfPhotonsOutTopOfTissue;
+            }
+            else if (dp.StateFlag.HasFlag(PhotonStateType.PseudoTransmittedTissueBoundary))
+            {
+                ++statistics.NumberOfPhotonsOutBottomOfTissue;
+            }
+            else if (dp.StateFlag.HasFlag(PhotonStateType.Absorbed))
+            {
+                ++statistics.NumberOfPhotonsAbsorbed;
+            }
+            else if (dp.StateFlag.HasFlag(PhotonStateType.PseudoSpecularTissueBoundary))
+            {
+                ++statistics.NumberOfPhotonsSpecularReflected;
+            }
+            else if (dp.StateFlag.HasFlag(PhotonStateType.KilledOverMaximumCollisions))
+            {
+                ++statistics.NumberOfPhotonsKilledOverMaximumCollisions;
+            }
+            else if (dp.StateFlag.HasFlag(PhotonStateType.KilledOverMaximumPathLength))
+            {
+                ++statistics.NumberOfPhotonsKilledOverMaximumPathLength;
+            }
+
             return statistics;
         }
     }
