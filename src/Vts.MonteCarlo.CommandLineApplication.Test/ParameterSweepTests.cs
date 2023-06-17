9af46304e35a478932a12e9f9224abe14cc8a7af
Lisa Malenfant <lisa@silverliningcomputing.com>
2023-06-16 22:34:30 -0700

    Added unit testing to MCCL and moved the CommandLine class from to a separate file

---
+++ src/Vts.MonteCarlo.CommandLineApplication.Test/ParameterSweepTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Vts.Common;
+
+namespace Vts.MonteCarlo.CommandLineApplication.Test
+{
+    [TestFixture]
+    internal class ParameterSweepTests
+    {
+        [Test]
+        public void ParameterSweep_default_constructor_test()
+        {
+            var parameterSweep = new ParameterSweep();
+            Assert.AreEqual("mua1", parameterSweep.Name);
+            Assert.AreEqual(0.01, parameterSweep.Range.Start);
+            Assert.AreEqual(0.05, parameterSweep.Range.Stop);
+            Assert.AreEqual(0.01, parameterSweep.Range.Delta);
+        }
+
+        [Test]
+        public void ParameterSweep_constructor_with_DoubleRange_test()
+        {
+            var parameterSweep = new ParameterSweep("mua1", new DoubleRange(0.0, 9.0, 10));
+            Assert.AreEqual("mua1", parameterSweep.Name);
+            Assert.AreEqual(0.0, parameterSweep.Range.Start);
+            Assert.AreEqual(9.0, parameterSweep.Range.Stop);
+            Assert.AreEqual(1.0, parameterSweep.Range.Delta);
+        }
+
+        [Test]
+        public void ParameterSweep_constructor_with_double_array_test()
+        {
+            var parameterSweep = new ParameterSweep("mua2", new [] { 0.1, 0.2, 0.3 });
+            Assert.AreEqual(0.1, parameterSweep.Values[0]);
+            Assert.AreEqual("mua2", parameterSweep.Name);
+        }
+    }
+}
