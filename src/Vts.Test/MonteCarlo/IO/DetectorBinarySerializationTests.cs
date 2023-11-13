9b11460d3d3e080a3eaf82acd04400eec3c4a4b4
hayakawa <hayakawa@uci.edu>
2023-11-13 13:08:54 -0800

    Finished unit tests for DetectorBinarySerialization in all detectors.  Need to try toggling TallySecondMoment and test that.

--- src/Vts.Test/MonteCarlo/IO/DetectorBinarySerializationTests.cs
+++ src/Vts.Test/MonteCarlo/IO/DetectorBinarySerializationTests.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Numerics;
+using System.Threading.Tasks;
 using Vts.Common;
 using Vts.IO;
+using Vts.MonteCarlo;
 using Vts.MonteCarlo.Detectors;
 using Vts.Test.MonteCarlo.IO;
 
@@ -3096,13 +3098,123 @@ Validate_TransmittedMTOfXAndYAndSubregionDetector_deserialized_class_is_correct_
         /// <summary>
         /// test to verify that GetBinarySerializers are working correctly for 4D detector.
         /// </summary>
+        [Test]
+        public void
+            Validate_FluenceOfXAndYAndZAndOmegaDetector_deserialized_class_is_correct_when_using_GetBinarySerializers()
+        {
+            const string detectorName = "testfluenceofxandyandzandOmega";
+            var detector = new FluenceOfXAndYAndZAndOmegaDetector
+            {
+                X = new DoubleRange(-10, 10, 3),
+                Y = new DoubleRange(-10, 10, 3),
+                Z = new DoubleRange(0, 1, 3),
+                Omega = new DoubleRange(0, 1, 3),
+                TallySecondMoment = true, // tally SecondMoment
+                Name = detectorName,
+                Mean = new Complex[,,,] // X.Count-1 x Y.Count-1 x Z.Count-1 x Omega.Count: 2x2x2x3
+                {
+                    {
+                        { 
+                            { 1 + Complex.ImaginaryOne, 2 + 2 * Complex.ImaginaryOne, 3 + 3 * Complex.ImaginaryOne }, 
+                            { 4 + 4 * Complex.ImaginaryOne, 5 + 5 * Complex.ImaginaryOne, 6 + 6 * Complex.ImaginaryOne }
+                        },
+                        { 
+                            { 7 + 7 * Complex.ImaginaryOne, 8 + 8 * Complex.ImaginaryOne, 9 + 9 * Complex.ImaginaryOne }, 
+                            { 10 + 10 * Complex.ImaginaryOne, 11 + 11 * Complex.ImaginaryOne, 12 + 12 * Complex.ImaginaryOne }
+                        }
+                    },
+                    {
+                        {
+                            { 13 + 13 * Complex.ImaginaryOne, 14 + 14 * Complex.ImaginaryOne, 15 + 15 * Complex.ImaginaryOne}, 
+                            { 16 + 16 * Complex.ImaginaryOne, 17 + 17 * Complex.ImaginaryOne, 18 + 18 * Complex.ImaginaryOne }
+                        },
+                        {
+                            { 19 + 19 * Complex.ImaginaryOne, 20 + 20 * Complex.ImaginaryOne, 21 + 21 * Complex.ImaginaryOne }, 
+                            { 22 + 22 * Complex.ImaginaryOne, 23 + 23 * Complex.ImaginaryOne, 24 + 24 * Complex.ImaginaryOne }
+                        }
+                    }
+                },
+                SecondMoment = new Complex[,,,]
+                {
+                    {
+                        {
+                            { 25 + 25 * Complex.ImaginaryOne, 26 + 26 * Complex.ImaginaryOne, 27 + 27 * Complex.ImaginaryOne}, 
+                            { 28 + 28 * Complex.ImaginaryOne, 29 + 29 * Complex.ImaginaryOne, 30 + 30 * Complex.ImaginaryOne}
+                        },
+                        {
+                            { 31 + 31 * Complex.ImaginaryOne, 32 + 32 * Complex.ImaginaryOne, 33 + 33 * Complex.ImaginaryOne }, 
+                            { 34 + 34 * Complex.ImaginaryOne, 35 + 35 * Complex.ImaginaryOne, 36 + 36 * Complex.ImaginaryOne }
+                        }
+                    },
+                    {
+                        {
+                            { 37 + 37 * Complex.ImaginaryOne, 38 + 38 * Complex.ImaginaryOne, 39 + 39 * Complex.ImaginaryOne }, 
+                            { 40 + 40 * Complex.ImaginaryOne, 41 + 41 * Complex.ImaginaryOne, 42 + 42 * Complex.ImaginaryOne }
+                        },
+                        {
+                            { 43 + 43 * Complex.ImaginaryOne, 44 + 44 * Complex.ImaginaryOne, 45 + 45 * Complex.ImaginaryOne }, 
+                            { 46 + 46 * Complex.ImaginaryOne, 47 + 47 * Complex.ImaginaryOne, 48 + 48 * Complex.ImaginaryOne }
+                        }
+                    }
+                }
+            };
 
+            DetectorBinarySerializationHelper.WriteClearAndReReadArrays(detector, detector.Mean, detector.SecondMoment);
 
+            Assert.AreEqual(1 + Complex.ImaginaryOne, detector.Mean[0, 0, 0, 0]);
+            Assert.AreEqual(2 + 2 * Complex.ImaginaryOne, detector.Mean[0, 0, 0, 1]);
+            Assert.AreEqual(3 + 3 * Complex.ImaginaryOne, detector.Mean[0, 0, 0, 2]);
+            Assert.AreEqual(4 + 4 * Complex.ImaginaryOne, detector.Mean[0, 0, 1, 0]);
+            Assert.AreEqual(5 + 5 * Complex.ImaginaryOne, detector.Mean[0, 0, 1, 1]);
+            Assert.AreEqual(6 + 6 * Complex.ImaginaryOne, detector.Mean[0, 0, 1, 2]);
+            Assert.AreEqual(7 + 7 * Complex.ImaginaryOne, detector.Mean[0, 1, 0, 0]);
+            Assert.AreEqual(8 + 8 * Complex.ImaginaryOne, detector.Mean[0, 1, 0, 1]);
+            Assert.AreEqual(9 + 9 * Complex.ImaginaryOne, detector.Mean[0, 1, 0, 2]);
+            Assert.AreEqual(10 + 10 * Complex.ImaginaryOne, detector.Mean[0, 1, 1, 0]);
+            Assert.AreEqual(11 + 11 * Complex.ImaginaryOne, detector.Mean[0, 1, 1, 1]);
+            Assert.AreEqual(12 + 12 * Complex.ImaginaryOne, detector.Mean[0, 1, 1, 2]);
+            Assert.AreEqual(13 + 13 * Complex.ImaginaryOne, detector.Mean[1, 0, 0, 0]);
+            Assert.AreEqual(14 + 14 * Complex.ImaginaryOne, detector.Mean[1, 0, 0, 1]);
+            Assert.AreEqual(15 + 15 * Complex.ImaginaryOne, detector.Mean[1, 0, 0, 2]);
+            Assert.AreEqual(16 + 16 * Complex.ImaginaryOne, detector.Mean[1, 0, 1, 0]);
+            Assert.AreEqual(17 + 17 * Complex.ImaginaryOne, detector.Mean[1, 0, 1, 1]);
+            Assert.AreEqual(18 + 18 * Complex.ImaginaryOne, detector.Mean[1, 0, 1, 2]);
+            Assert.AreEqual(19 + 19 * Complex.ImaginaryOne, detector.Mean[1, 1, 0, 0]);
+            Assert.AreEqual(20 + 20 * Complex.ImaginaryOne, detector.Mean[1, 1, 0, 1]);
+            Assert.AreEqual(21 + 21 * Complex.ImaginaryOne, detector.Mean[1, 1, 0, 2]);
+            Assert.AreEqual(22 + 22 * Complex.ImaginaryOne, detector.Mean[1, 1, 1, 0]);
+            Assert.AreEqual(23 + 23 * Complex.ImaginaryOne, detector.Mean[1, 1, 1, 1]);
+            Assert.AreEqual(24 + 24 * Complex.ImaginaryOne, detector.Mean[1, 1, 1, 2]);
+            Assert.AreEqual(25 + 25 * Complex.ImaginaryOne, detector.SecondMoment[0, 0, 0, 0]);
+            Assert.AreEqual(26 + 26 * Complex.ImaginaryOne, detector.SecondMoment[0, 0, 0, 1]);
+            Assert.AreEqual(27 + 27 * Complex.ImaginaryOne, detector.SecondMoment[0, 0, 0, 2]);
+            Assert.AreEqual(28 + 28 * Complex.ImaginaryOne, detector.SecondMoment[0, 0, 1, 0]);
+            Assert.AreEqual(29 + 29 * Complex.ImaginaryOne, detector.SecondMoment[0, 0, 1, 1]);
+            Assert.AreEqual(30 + 30 * Complex.ImaginaryOne, detector.SecondMoment[0, 0, 1, 2]);
+            Assert.AreEqual(31 + 31 * Complex.ImaginaryOne, detector.SecondMoment[0, 1, 0, 0]);
+            Assert.AreEqual(32 + 32 * Complex.ImaginaryOne, detector.SecondMoment[0, 1, 0, 1]);
+            Assert.AreEqual(33 + 33 * Complex.ImaginaryOne, detector.SecondMoment[0, 1, 0, 2]);
+            Assert.AreEqual(34 + 34 * Complex.ImaginaryOne, detector.SecondMoment[0, 1, 1, 0]);
+            Assert.AreEqual(35 + 35 * Complex.ImaginaryOne, detector.SecondMoment[0, 1, 1, 1]);
+            Assert.AreEqual(36 + 36 * Complex.ImaginaryOne, detector.SecondMoment[0, 1, 1, 2]);
+            Assert.AreEqual(37 + 37 * Complex.ImaginaryOne, detector.SecondMoment[1, 0, 0, 0]);
+            Assert.AreEqual(38 + 38 * Complex.ImaginaryOne, detector.SecondMoment[1, 0, 0, 1]);
+            Assert.AreEqual(39 + 39 * Complex.ImaginaryOne, detector.SecondMoment[1, 0, 0, 2]);
+            Assert.AreEqual(40 + 40 * Complex.ImaginaryOne, detector.SecondMoment[1, 0, 1, 0]);
+            Assert.AreEqual(41 + 41 * Complex.ImaginaryOne, detector.SecondMoment[1, 0, 1, 1]);
+            Assert.AreEqual(42 + 42 * Complex.ImaginaryOne, detector.SecondMoment[1, 0, 1, 2]);
+            Assert.AreEqual(43 + 43 * Complex.ImaginaryOne, detector.SecondMoment[1, 1, 0, 0]);
+            Assert.AreEqual(44 + 44 * Complex.ImaginaryOne, detector.SecondMoment[1, 1, 0, 1]);
+            Assert.AreEqual(45 + 45 * Complex.ImaginaryOne, detector.SecondMoment[1, 1, 0, 2]);
+            Assert.AreEqual(46 + 46 * Complex.ImaginaryOne, detector.SecondMoment[1, 1, 1, 0]);
+            Assert.AreEqual(47 + 47 * Complex.ImaginaryOne, detector.SecondMoment[1, 1, 1, 1]);
+            Assert.AreEqual(48 + 48 * Complex.ImaginaryOne, detector.SecondMoment[1, 1, 1, 2]);
+        }
         [Test]
         public void
             Validate_FluenceOfXAndYAndZAndTimeDetector_deserialized_class_is_correct_when_using_GetBinarySerializers()
         {
-            const string detectorName = "testaofxandyandzandtime";
+            const string detectorName = "testfluenceofxandyandzandtime";
             var detector = new FluenceOfXAndYAndZAndTimeDetector
             {
                 X = new DoubleRange(-10, 10, 3),
@@ -3685,10 +3797,108 @@ Validate_TransmittedMTOfXAndYAndSubregionDetector_deserialized_class_is_correct_
             Assert.AreEqual(55, detector.ROfXAndYSecondMoment[1, 0]);
             Assert.AreEqual(56, detector.ROfXAndYSecondMoment[1, 1]);
         }
+        [Test]
+        public void
+           Validate_TOfXAndYAndTimeAndSubregionDetector_deserialized_class_is_correct_when_using_GetBinarySerializers()
+        {
+            const string detectorName = "testTofxandyandtimeandsubregion";
+            var detector = new TOfXAndYAndTimeAndSubregionDetector
+            {
+                X = new DoubleRange(-10, 10, 3),
+                Y = new DoubleRange(-10, 10, 3),
+                Time = new DoubleRange(0, 1, 3),
+                TallySecondMoment = true, // tally SecondMoment
+                Name = detectorName,
+                Mean = new double[,,,] // X.Count-1 x Y.Count-1 x Time.Count-1 x NumOfRegions: 2x2x2x3
+                {
+                    {
+                        { { 1, 2, 3 }, { 4, 5, 6 } },
+                        { { 7, 8, 9 }, { 10, 11, 12 } }
+                    },
+                    {
+                        { { 13, 14, 15 }, { 16, 17, 18 } },
+                        { { 19, 20, 21 }, { 22, 23, 24 } }
+                    }
+                },
+                SecondMoment = new double[,,,]
+                {
+                    {
+                        { { 25, 26, 27 }, { 28, 29, 30 } },
+                        { { 31, 32, 33 }, { 34, 35, 36 } }
+                    },
+                    {
+                        { { 37, 38, 39 }, { 40, 41, 42 } },
+                        { { 43, 44, 45 }, { 46, 47, 48 } }
+                    }
+                },
+                TOfXAndY = new double[,] // X.Count-1 x Y.Count-1: 2x2
+                    { { 49, 50 }, { 51, 52 } },
+                TOfXAndYSecondMoment = new double[,]
+                    { { 53, 54 }, { 55, 56 } }
+            };
 
+            DetectorBinarySerializationHelper.WriteClearAndReReadArrays(detector, detector.Mean, detector.SecondMoment,
+                detector.TOfXAndY, detector.TOfXAndYSecondMoment);
+
+            Assert.AreEqual(1, detector.Mean[0, 0, 0, 0]);
+            Assert.AreEqual(2, detector.Mean[0, 0, 0, 1]);
+            Assert.AreEqual(3, detector.Mean[0, 0, 0, 2]);
+            Assert.AreEqual(4, detector.Mean[0, 0, 1, 0]);
+            Assert.AreEqual(5, detector.Mean[0, 0, 1, 1]);
+            Assert.AreEqual(6, detector.Mean[0, 0, 1, 2]);
+            Assert.AreEqual(7, detector.Mean[0, 1, 0, 0]);
+            Assert.AreEqual(8, detector.Mean[0, 1, 0, 1]);
+            Assert.AreEqual(9, detector.Mean[0, 1, 0, 2]);
+            Assert.AreEqual(10, detector.Mean[0, 1, 1, 0]);
+            Assert.AreEqual(11, detector.Mean[0, 1, 1, 1]);
+            Assert.AreEqual(12, detector.Mean[0, 1, 1, 2]);
+            Assert.AreEqual(13, detector.Mean[1, 0, 0, 0]);
+            Assert.AreEqual(14, detector.Mean[1, 0, 0, 1]);
+            Assert.AreEqual(15, detector.Mean[1, 0, 0, 2]);
+            Assert.AreEqual(16, detector.Mean[1, 0, 1, 0]);
+            Assert.AreEqual(17, detector.Mean[1, 0, 1, 1]);
+            Assert.AreEqual(18, detector.Mean[1, 0, 1, 2]);
+            Assert.AreEqual(19, detector.Mean[1, 1, 0, 0]);
+            Assert.AreEqual(20, detector.Mean[1, 1, 0, 1]);
+            Assert.AreEqual(21, detector.Mean[1, 1, 0, 2]);
+            Assert.AreEqual(22, detector.Mean[1, 1, 1, 0]);
+            Assert.AreEqual(23, detector.Mean[1, 1, 1, 1]);
+            Assert.AreEqual(24, detector.Mean[1, 1, 1, 2]);
+            Assert.AreEqual(25, detector.SecondMoment[0, 0, 0, 0]);
+            Assert.AreEqual(26, detector.SecondMoment[0, 0, 0, 1]);
+            Assert.AreEqual(27, detector.SecondMoment[0, 0, 0, 2]);
+            Assert.AreEqual(28, detector.SecondMoment[0, 0, 1, 0]);
+            Assert.AreEqual(29, detector.SecondMoment[0, 0, 1, 1]);
+            Assert.AreEqual(30, detector.SecondMoment[0, 0, 1, 2]);
+            Assert.AreEqual(31, detector.SecondMoment[0, 1, 0, 0]);
+            Assert.AreEqual(32, detector.SecondMoment[0, 1, 0, 1]);
+            Assert.AreEqual(33, detector.SecondMoment[0, 1, 0, 2]);
+            Assert.AreEqual(34, detector.SecondMoment[0, 1, 1, 0]);
+            Assert.AreEqual(35, detector.SecondMoment[0, 1, 1, 1]);
+            Assert.AreEqual(36, detector.SecondMoment[0, 1, 1, 2]);
+            Assert.AreEqual(37, detector.SecondMoment[1, 0, 0, 0]);
+            Assert.AreEqual(38, detector.SecondMoment[1, 0, 0, 1]);
+            Assert.AreEqual(39, detector.SecondMoment[1, 0, 0, 2]);
+            Assert.AreEqual(40, detector.SecondMoment[1, 0, 1, 0]);
+            Assert.AreEqual(41, detector.SecondMoment[1, 0, 1, 1]);
+            Assert.AreEqual(42, detector.SecondMoment[1, 0, 1, 2]);
+            Assert.AreEqual(43, detector.SecondMoment[1, 1, 0, 0]);
+            Assert.AreEqual(44, detector.SecondMoment[1, 1, 0, 1]);
+            Assert.AreEqual(45, detector.SecondMoment[1, 1, 0, 2]);
+            Assert.AreEqual(46, detector.SecondMoment[1, 1, 1, 0]);
+            Assert.AreEqual(47, detector.SecondMoment[1, 1, 1, 1]);
+            Assert.AreEqual(48, detector.SecondMoment[1, 1, 1, 2]);
+            Assert.AreEqual(49, detector.TOfXAndY[0, 0]);
+            Assert.AreEqual(50, detector.TOfXAndY[0, 1]);
+            Assert.AreEqual(51, detector.TOfXAndY[1, 0]);
+            Assert.AreEqual(52, detector.TOfXAndY[1, 1]);
+            Assert.AreEqual(53, detector.TOfXAndYSecondMoment[0, 0]);
+            Assert.AreEqual(54, detector.TOfXAndYSecondMoment[0, 1]);
+            Assert.AreEqual(55, detector.TOfXAndYSecondMoment[1, 0]);
+            Assert.AreEqual(56, detector.TOfXAndYSecondMoment[1, 1]);
+        }
 
         #endregion
-
 
         #region 5D detectors: complete
 
