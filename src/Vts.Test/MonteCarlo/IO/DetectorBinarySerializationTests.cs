1ebf77f9fb12baeafee3c02b589697ce2d03aae1
hayakawa <hayakawa@uci.edu>
2023-11-14 19:10:44 -0800

    Added missing unit test for pMCTOfRhoDetector.

--- src/Vts.Test/MonteCarlo/IO/DetectorBinarySerializationTests.cs
+++ src/Vts.Test/MonteCarlo/IO/DetectorBinarySerializationTests.cs
@@ -420,6 +420,33 @@ namespace Vts.Test.MonteCarlo
             Assert.AreEqual(5, detector.SecondMoment[1]);
             Assert.AreEqual(6, detector.SecondMoment[2]);
         }
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void Validate_pMCTOfRhoDetector_deserialized_class_is_correct_when_using_GetBinarySerializers(bool tallySecondMoment)
+        {
+            const string detectorName = "testpmctofrho";
+            var detector = new pMCTOfRhoDetector
+            {
+                Rho = new DoubleRange(0, 10, 3),
+                TallySecondMoment = tallySecondMoment,
+                PerturbedOps = new List<OpticalProperties> { new() },
+                PerturbedRegionsIndices = new List<int> { 1 },
+                Name = detectorName,
+                Mean = new double[] { 1, 2, 3 },
+                SecondMoment = new double[] { 4, 5, 6 }
+            };
+
+            DetectorBinarySerializationHelper.WriteClearAndReReadArrays(detector, detector.Mean, detector.SecondMoment);
+
+            Assert.AreEqual(1, detector.Mean[0]);
+            Assert.AreEqual(2, detector.Mean[1]);
+            Assert.AreEqual(3, detector.Mean[2]);
+            if (!tallySecondMoment) return;
+            Assert.AreEqual(4, detector.SecondMoment[0]);
+            Assert.AreEqual(5, detector.SecondMoment[1]);
+            Assert.AreEqual(6, detector.SecondMoment[2]);
+        }
 
         [Test]
         [TestCase(true)]
