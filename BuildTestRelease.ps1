76c7c3f52c8ff9328277daa2fe1fe2c6d37834ff
hayakawa <hayakawa@uci.edu>
2024-04-18 16:24:56 -0700

    Added check if Matlab installed before running powershell commands that rely on its installation.

--- BuildTestRelease.ps1
+++ BuildTestRelease.ps1
@@ -2,6 +2,10 @@ $mccl_version = "7.3.0"
 $matlab_version = "10.0.0"
 
 Invoke-Expression ".\BuildTestReleaseMCCL.ps1 $mccl_version"
-Invoke-Expression ".\BuildTestReleaseMATLAB.ps1 $matlab_version"
+# only run if matlab installed
+if (Get-Command "matlab" -ErrorAction SilentlyContinue) 
+{
+  Invoke-Expression ".\BuildTestReleaseMATLAB.ps1 $matlab_version"
+}
 
 Read-Host -Prompt "Press Enter to exit"
