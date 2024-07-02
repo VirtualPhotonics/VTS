39dee818dda334833d9791f56cddbad486576bf5
hayakawa <hayakawa@uci.edu>
2024-07-02 15:47:08 -0700

    Added unit tests to validate newly added All*Fiber*Detector arrays added to SimulationOutput.

--- src/Vts.Test/MonteCarlo/DataStructures/SimulationOutputTests.cs
+++ src/Vts.Test/MonteCarlo/DataStructures/SimulationOutputTests.cs
@@ -19,7 +19,7 @@ namespace Vts.Test.MonteCarlo
             var detectorList =
                 new List<IDetector>
                 {
-                    new ROfAngleDetector()
+                    new ROfAngleDetector
                     {
                          Angle = new DoubleRange(0, Math.PI, 10),
                          Name = "testName"
@@ -33,6 +33,200 @@ namespace Vts.Test.MonteCarlo
             Assert.AreEqual(0d, angle.Start);
             Assert.AreEqual(Math.PI, angle.Stop);
             Assert.AreEqual(10, angle.Count);
+        }
+
+        /// <summary>
+        /// Test to check that created arrays for surface fiber detectors that allow multiple fibers
+        /// to be defined in a single simulation (rather than having to run a separate simulation for each
+        /// fiber definition) are correct.
+        /// </summary>
+        [Test]
+        public void Validate_arrays_for_multiple_SurfaceFiber_detectors_are_correct()
+        {
+            var detectorList =
+                new List<IDetector>
+                {
+                    new SurfaceFiberDetector
+                    {
+                        Center = new Position(0, 0, 1.0),
+                        Radius = 1.0,
+                        TallySecondMoment = true,
+                        N = 1.0,
+                        NA = 1.0,
+                        FinalTissueRegionIndex = 1,
+                        Mean = 1.0,
+                        SecondMoment = 2.0,
+                        TallyCount = 3,
+                        Name = "SurfaceFiber1"
+                    },
+                    new SurfaceFiberDetector
+                    {
+                        Center = new Position(0, 0, 2.0),
+                        Radius = 2.0,
+                        TallySecondMoment = true,
+                        N = 1.2,
+                        NA = 1.2,
+                        FinalTissueRegionIndex = 2,
+                        Mean = 2.0,
+                        SecondMoment = 4.0,
+                        TallyCount = 6,
+                        Name = "SurfaceFiber2"
+                    },
+
+                };
+            var output = new SimulationOutput(new SimulationInput(), detectorList);
+
+            // verify first detector specification
+            var detector = (SurfaceFiberDetector)output.ResultsDictionary["SurfaceFiber1"];
+            Assert.AreEqual(1.0, detector.Center.Z);
+            Assert.AreEqual(1.0, detector.Radius);
+            Assert.AreEqual(1.0, detector.N);
+            Assert.AreEqual(1.0, detector.NA);
+            Assert.AreEqual(1, detector.FinalTissueRegionIndex);
+            Assert.AreEqual(1.0, detector.Mean);
+            Assert.AreEqual(2.0, detector.SecondMoment);
+            Assert.AreEqual(3, detector.TallyCount);
+
+            // verify second detector specification
+            detector = (SurfaceFiberDetector)output.ResultsDictionary["SurfaceFiber2"];
+            Assert.AreEqual(2.0, detector.Center.Z);
+            Assert.AreEqual(2.0, detector.Radius);
+            Assert.AreEqual(1.2, detector.N);
+            Assert.AreEqual(1.2, detector.NA);
+            Assert.AreEqual(2, detector.FinalTissueRegionIndex);
+            Assert.AreEqual(2.0, detector.Mean);
+            Assert.AreEqual(4.0, detector.SecondMoment);
+            Assert.AreEqual(6, detector.TallyCount);
+        }
+
+        /// <summary>
+        /// Test to check that created arrays for slanted recessed fiber detectors that allow multiple fibers
+        /// to be defined in a single simulation (rather than having to run a separate simulation for each
+        /// fiber definition) are correct.
+        /// </summary>
+        [Test]
+        public void Validate_arrays_for_multiple_SlantedRecessedFiber_detectors_are_correct()
+        {
+            var detectorList =
+                new List<IDetector>
+                {
+                    new SlantedRecessedFiberDetector
+                    {
+                        Center = new Position(0, 0, 1.0),
+                        Radius = 1.0,
+                        TallySecondMoment = true,
+                        N = 1.0,
+                        NA = 1.0,
+                        FinalTissueRegionIndex = 1,
+                        Mean = 1.0,
+                        SecondMoment = 2.0,
+                        TallyCount = 3,
+                        Name = "SlantedRecessedFiber1"
+                    },
+                    new SlantedRecessedFiberDetector
+                    {
+                        Center = new Position(0, 0, 2.0),
+                        Radius = 2.0,
+                        TallySecondMoment = true,
+                        N = 1.2,
+                        NA = 1.2,
+                        FinalTissueRegionIndex = 2,                        
+                        Mean = 2.0,
+                        SecondMoment = 4.0,
+                        TallyCount = 6,
+                        Name = "SlantedRecessedFiber2"
+                    },
+
+                };
+            var output = new SimulationOutput(new SimulationInput(), detectorList);
+
+            // verify first detector specification
+            var detector = (SlantedRecessedFiberDetector)output.ResultsDictionary["SlantedRecessedFiber1"];
+            Assert.AreEqual(1.0, detector.Center.Z);
+            Assert.AreEqual(1.0, detector.Radius);
+            Assert.AreEqual(1.0, detector.N);
+            Assert.AreEqual(1.0, detector.NA);
+            Assert.AreEqual(1, detector.FinalTissueRegionIndex);
+            Assert.AreEqual(1.0, detector.Mean);
+            Assert.AreEqual(2.0, detector.SecondMoment);
+            Assert.AreEqual(3, detector.TallyCount);
+
+            // verify second detector specification
+            detector = (SlantedRecessedFiberDetector)output.ResultsDictionary["SlantedRecessedFiber2"];
+            Assert.AreEqual(2.0, detector.Center.Z);
+            Assert.AreEqual(2.0, detector.Radius);
+            Assert.AreEqual(1.2, detector.N);
+            Assert.AreEqual(1.2, detector.NA);
+            Assert.AreEqual(2, detector.FinalTissueRegionIndex);
+            Assert.AreEqual(2.0, detector.Mean);
+            Assert.AreEqual(4.0, detector.SecondMoment);
+            Assert.AreEqual(6, detector.TallyCount);
+        }
+
+        /// <summary>
+        /// Test to check that created arrays for internal surface fiber detectors that allow multiple fibers
+        /// to be defined in a single simulation (rather than having to run a separate simulation for each
+        /// fiber definition) are correct.
+        /// </summary>
+        [Test]
+        public void Validate_arrays_for_multiple_InternalSurfaceFiber_detectors_are_correct()
+        {
+            var detectorList =
+                new List<IDetector>
+                {
+                    new InternalSurfaceFiberDetector 
+                    {
+                        Center = new Position(0, 0, 1.0),
+                        Radius = 1.0,
+                        TallySecondMoment = true,
+                        N = 1.0,
+                        NA = 1.0,
+                        FinalTissueRegionIndex = 1,
+                        InDirectionOfFiberAxis = new Direction(0, 0, -1),
+                        Mean = 1.0,
+                        SecondMoment = 2.0,
+                        TallyCount = 3,
+                        Name = "InternalSurfaceFiber1"
+                    },
+                    new InternalSurfaceFiberDetector
+                    {
+                        Center = new Position(0, 0, 2.0),
+                        Radius = 2.0,
+                        TallySecondMoment = true,
+                        N = 1.2,
+                        NA = 1.2, 
+                        FinalTissueRegionIndex = 2,
+                        InDirectionOfFiberAxis = new Direction(0, 0, -1),
+                        Mean = 2.0,
+                        SecondMoment = 4.0,
+                        TallyCount = 6,
+                        Name = "InternalSurfaceFiber2"
+                    },
+
+                };
+            var output = new SimulationOutput(new SimulationInput(), detectorList);
+
+            // verify first detector specification
+            var detector = (InternalSurfaceFiberDetector)output.ResultsDictionary["InternalSurfaceFiber1"];
+            Assert.AreEqual(1.0, detector.Center.Z);
+            Assert.AreEqual(1.0, detector.Radius);
+            Assert.AreEqual(1.0, detector.N);
+            Assert.AreEqual(1.0, detector.NA);
+            Assert.AreEqual(1, detector.FinalTissueRegionIndex);
+            Assert.AreEqual(1.0, detector.Mean);
+            Assert.AreEqual(2.0, detector.SecondMoment);
+            Assert.AreEqual(3, detector.TallyCount);
+
+            // verify second detector specification
+            detector = (InternalSurfaceFiberDetector)output.ResultsDictionary["InternalSurfaceFiber2"];
+            Assert.AreEqual(2.0, detector.Center.Z);
+            Assert.AreEqual(2.0, detector.Radius);
+            Assert.AreEqual(1.2, detector.N);
+            Assert.AreEqual(1.2, detector.NA);
+            Assert.AreEqual(2, detector.FinalTissueRegionIndex);
+            Assert.AreEqual(2.0, detector.Mean);
+            Assert.AreEqual(4.0, detector.SecondMoment);
+            Assert.AreEqual(6, detector.TallyCount);
         }
 
     }
