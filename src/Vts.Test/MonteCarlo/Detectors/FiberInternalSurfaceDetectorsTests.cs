228e666b796fa0b4f441cd7e16482a6a2d6f9d04
hayakawa <hayakawa@uci.edu>
2024-11-07 15:52:17 -0800

    Reorganized unit tests to put easier ones first to see if they pass on Linux.

--- src/Vts.Test/MonteCarlo/Detectors/FiberInternalSurfaceDetectorsTests.cs
+++ src/Vts.Test/MonteCarlo/Detectors/FiberInternalSurfaceDetectorsTests.cs
@@ -17,16 +17,17 @@ namespace Vts.Test.MonteCarlo.Detectors
     {
         private SimulationOptions _simulationOptions;
         private ISourceInput _sourceOutsideSurface;
-        private ITissueInput _tissueWithCylinders, _tissueWithLayers, _tissueWithAirLayer;
-        private IList<IDetectorInput> _detectorsCylinderSystem,
-            _detectorAtopSecondLayerOpen;
-        private SimulationOutput _outputCylinderSystem,
-            _outputAtopSecondTissueLayer, 
-            _outputAtopSecondAirLayer;
-        private const double DetectorRadius = 1; // make big so catch more
+        private ITissueInput _tissueWithLayers, _tissueWithAirLayer, _tissueWithCylinders;
+        private IList<IDetectorInput> _detectorAtopSecondLayerOpen, _detectorsCylinderSystem;
+        private SimulationInput _inputAtopSecondTissueLayer, _inputAtopSecondAirLayer, _inputCylinderSystem;
+        private SimulationOutput _outputAtopSecondTissueLayer, _outputAtopSecondAirLayer, _outputCylinderSystem;
+        private const double DetectorAtopCylinderRadius = 1.0; // make bigger so catch more
 
         /// <summary>
-        /// Setup input to the MC for a homogeneous one layer tissue with 
+        /// Setup input to the MC for:
+        /// Case 1) two layer tissue (air-layer-layer-air) with source and detector atop 2nd layer,
+        /// Case 2) two layer tissue (air-air-layer-air) with source and detector atop 2nd layer, and
+        /// Case 3) homogeneous one layer tissue with 
         /// multi-concentric infinite cylinder and put fibers on outside outermost cylinder surface
         /// (dot product with surface normal is positive) and inside innermost cylinder surface
         /// (dot product with surface normal is negative) and make sure detection is correct
@@ -40,14 +41,83 @@ namespace Vts.Test.MonteCarlo.Detectors
                 RandomNumberGeneratorType.MersenneTwister,
                 AbsorptionWeightingType.Discrete,
                 PhaseFunctionType.HenyeyGreenstein,
-                new List<DatabaseType>(), 
+                new List<DatabaseType>(),
                 false, // track statistics
                 0.0, // RR threshold -> 0 = no RR performed
                 0);
             _sourceOutsideSurface = new DirectionalPointSourceInput(
-                     new Position(0.0, 0.0, 1.0), // at surface of outer cyl and 2nd "tissue" layer
-                     new Direction(0.0, 0.0, 1.0), 
-                     1);
+                new Position(0.0, 0.0, 1.0), // at surface of 2nd "tissue" layer or outer cylinder
+                new Direction(0.0, 0.0, 1.0),
+                1); // in top tissue layer or surrounding layer of cylinder
+
+            // Specify air-tissue-tissue-air system with internal fiber atop second layer
+            _detectorAtopSecondLayerOpen = new List<IDetectorInput>
+            {
+                new InternalSurfaceFiberDetectorInput
+                {
+                    Center = new Position(0, 0, 1), // atop 2nd "tissue" layer
+                    Radius = 1.0,
+                    TallySecondMoment = true,
+                    N = 1.4,
+                    NA = 1.4,
+                    FinalTissueRegionIndex = 1, // atop 2nd "tissue" layer
+                    InDirectionOfFiberAxis = new Direction(0, 0, -1)
+                }
+            };
+            _tissueWithLayers =
+                new MultiLayerTissueInput(
+                    [
+                        new LayerTissueRegion(
+                            new DoubleRange(double.NegativeInfinity, 0.0),
+                            new OpticalProperties(0.0, 1e-10, 1.0, 1.0)),
+                        new LayerTissueRegion( // tissue in top layer
+                            new DoubleRange(0.0, 1.0),
+                            new OpticalProperties(0.01, 1.0, 0.8, 1.4)),
+                        new LayerTissueRegion(
+                            new DoubleRange(1.0, 100.0),
+                            new OpticalProperties(0.01, 1.0, 0.8, 1.4)),
+                        new LayerTissueRegion(
+                            new DoubleRange(100.0, double.PositiveInfinity),
+                            new OpticalProperties(0.0, 1e-10, 1.0, 1.0))
+                    ]
+                );
+            _inputAtopSecondTissueLayer = new SimulationInput(
+                100,
+                "",
+                _simulationOptions,
+                _sourceOutsideSurface,
+                _tissueWithLayers,
+                _detectorAtopSecondLayerOpen); // this is below bottom of 2nd air layer
+            _outputAtopSecondTissueLayer = new MonteCarloSimulation(_inputAtopSecondTissueLayer).Run();
+
+            // Specify air-air-tissue-air system with internal fiber atop second layer
+            _tissueWithAirLayer =
+                new MultiLayerTissueInput(
+                    [
+                        new LayerTissueRegion(
+                            new DoubleRange(double.NegativeInfinity, 0.0),
+                            new OpticalProperties(0.0, 1e-10, 1.0, 1.0)),
+                        new LayerTissueRegion( // put "air" as first layer nonsensical but unit test
+                            new DoubleRange(0.0, 1.0),
+                            new OpticalProperties(1E-100, 1E-10, 1.0, 1.0)),
+                        new LayerTissueRegion(
+                            new DoubleRange(1.0, 100.0),
+                            new OpticalProperties(0.01, 1.0, 0.8, 1.4)),
+                        new LayerTissueRegion(
+                            new DoubleRange(100.0, double.PositiveInfinity),
+                            new OpticalProperties(0.0, 1e-10, 1.0, 1.0))
+                    ]
+                );
+            _inputAtopSecondAirLayer = new SimulationInput(
+                100,
+                "",
+                _simulationOptions,
+                _sourceOutsideSurface,
+                _tissueWithAirLayer,
+                _detectorAtopSecondLayerOpen); // this is below bottom of 2nd air layer
+            _outputAtopSecondAirLayer = new MonteCarloSimulation(_inputAtopSecondAirLayer).Run();
+
+            // Specify air-tissue(with 2 concentric cylinders)-air system with internal fiber atop outer cylinder
             _tissueWithCylinders =
                 new MultiConcentricInfiniteCylinderTissueInput(
                     new ITissueRegion[]
@@ -74,13 +144,15 @@ namespace Vts.Test.MonteCarlo.Detectors
                             new OpticalProperties(0.0, 1e-10, 1.0, 1.0))
                     }
                 );
+
+            // Case 3) Concentric cylinders inside single layer of tissue
             // using SimulationOutput AllInternalFiberDetectorMeans definition
             _detectorsCylinderSystem = new List<IDetectorInput>
             {
                 new InternalSurfaceFiberDetectorInput // outer cylinder dot normal positive open
                 {
                     Center = new Position(0, 0, 1.0),
-                    Radius = DetectorRadius,
+                    Radius = DetectorAtopCylinderRadius,
                     TallySecondMoment = true,
                     N = 1.4,
                     NA = 1.4,
@@ -91,7 +163,7 @@ namespace Vts.Test.MonteCarlo.Detectors
                 new InternalSurfaceFiberDetectorInput // outer cylinder dot normal positive NA
                 {
                     Center = new Position(0, 0, 1.0),
-                    Radius = DetectorRadius,
+                    Radius = DetectorAtopCylinderRadius,
                     TallySecondMoment = true,
                     N = 1.4,
                     NA = 1.0, // tried many values here to obtain different tally than open, either all or nothing I found
@@ -102,107 +174,62 @@ namespace Vts.Test.MonteCarlo.Detectors
                 new InternalSurfaceFiberDetectorInput // inner cylinder dot normal negative open
                 {
                     Center = new Position(0, 0, 2.0),
-                    Radius = DetectorRadius,
+                    Radius = DetectorAtopCylinderRadius,
                     TallySecondMoment = true,
                     N = 1.4,
                     NA = 1.4,
                     FinalTissueRegionIndex = 4,
-                    InDirectionOfFiberAxis = new Direction(0,0, 1),
+                    InDirectionOfFiberAxis = new Direction(0, 0, 1),
                     Name = "InternalSurfaceFiber3"
                 },
                 new InternalSurfaceFiberDetectorInput // inner cylinder dot normal negative NA
                 {
                     Center = new Position(0, 0, 2.0),
-                    Radius = DetectorRadius,
+                    Radius = DetectorAtopCylinderRadius,
                     TallySecondMoment = true,
                     N = 1.4,
                     NA = 1.2, // make smaller but not too small so some still tally but different than open
                     FinalTissueRegionIndex = 4,
-                    InDirectionOfFiberAxis = new Direction(0,0, 1),
+                    InDirectionOfFiberAxis = new Direction(0, 0, 1),
                     Name = "InternalSurfaceFiber4"
                 }
             };
-
-            var input = new SimulationInput(
+            _inputCylinderSystem = new SimulationInput(
                 100,
                 "",
                 _simulationOptions,
                 _sourceOutsideSurface,
                 _tissueWithCylinders,
                 _detectorsCylinderSystem);
-            _outputCylinderSystem = new MonteCarloSimulation(input).Run();
-
-            // Specify layers with internal fiber atop second layer
-            _detectorAtopSecondLayerOpen = new List<IDetectorInput>
-            {
-                new InternalSurfaceFiberDetectorInput
-                {
-                    Center = new Position(0, 0, 1), // atop 2nd "tissue" layer
-                    Radius = 1.0, 
-                    TallySecondMoment = true,
-                    N = 1.4,
-                    NA = 1.4,
-                    FinalTissueRegionIndex = 1, // atop 2nd "tissue" layer
-                    InDirectionOfFiberAxis = new Direction(0, 0, -1)
-                }
-            };
-            _tissueWithLayers =
-                new MultiLayerTissueInput(
-                    new ITissueRegion[]
-                    {
-                        new LayerTissueRegion(
-                            new DoubleRange(double.NegativeInfinity, 0.0),
-                            new OpticalProperties(0.0, 1e-10, 1.0, 1.0)),
-                        new LayerTissueRegion( // tissue in top layer
-                            new DoubleRange(0.0, 1.0),
-                            new OpticalProperties(0.01, 1.0, 0.8, 1.4)),
-                        new LayerTissueRegion(
-                            new DoubleRange(1.0, 100.0),
-                            new OpticalProperties(0.01, 1.0, 0.8, 1.4)),
-                        new LayerTissueRegion(
-                            new DoubleRange(100.0, double.PositiveInfinity),
-                            new OpticalProperties(0.0, 1e-10, 1.0, 1.0))
-                    }
-                );
-            input = new SimulationInput(
-                100,
-                "",
-                _simulationOptions,
-                _sourceOutsideSurface,
-                _tissueWithLayers,
-                _detectorAtopSecondLayerOpen); // this is below bottom of 2nd air layer
-            _outputAtopSecondTissueLayer = new MonteCarloSimulation(input).Run();
-
-            _tissueWithAirLayer =
-                new MultiLayerTissueInput(
-                    new ITissueRegion[]
-                    {
-                        new LayerTissueRegion(
-                            new DoubleRange(double.NegativeInfinity, 0.0),
-                            new OpticalProperties(0.0, 1e-10, 1.0, 1.0)),
-                        new LayerTissueRegion( // put "air" as first layer nonsensical but unit test
-                            new DoubleRange(0.0, 1.0),
-                            new OpticalProperties(1E-100, 1E-10, 1.0, 1.0)),
-                        new LayerTissueRegion(
-                            new DoubleRange(1.0, 100.0),
-                            new OpticalProperties(0.01, 1.0, 0.8, 1.4)),
-                        new LayerTissueRegion(
-                            new DoubleRange(100.0, double.PositiveInfinity),
-                            new OpticalProperties(0.0, 1e-10, 1.0, 1.0))
-                    }
-                );
-            input = new SimulationInput(
-                100,
-                "",
-                _simulationOptions,
-                _sourceOutsideSurface,
-                _tissueWithAirLayer,
-                _detectorAtopSecondLayerOpen); // this is below bottom of 2nd air layer
-            _outputAtopSecondAirLayer = new MonteCarloSimulation(input).Run();
+            _outputCylinderSystem = new MonteCarloSimulation(_inputCylinderSystem).Run();
         }
 
         /// <summary>
-        /// Test to validate fiber at outer cylinder surface fully open with dot normal positive. 
+        /// Case 1) Test to validate fiber at 2nd tissue layer surface fully open with dot normal positive. 
+        /// Validation values based on prior test.
+        /// </summary>
+        [Test]
+        public void Validate_fully_open_fiber_atop_second_layer_below_tissue_produces_correct_results()
+        {
+            Assert.Less(Math.Abs(_outputAtopSecondTissueLayer.IntSurFib - 0.073386), 0.000001);
+            Assert.Less(Math.Abs(_outputAtopSecondTissueLayer.IntSurFib2 - 0.022474), 0.000001);
+            Assert.AreEqual(24, _outputAtopSecondTissueLayer.IntSurFib_TallyCount);
+        }
+
+        /// <summary>
+        /// Case 2) Test to validate fiber at 2nd air layer surface fully open with dot normal positive. 
+        /// Validation values based on prior test.
+        /// </summary>
+        [Test]
+        public void Validate_fully_open_fiber_atop_second_layer_below_air_produces_correct_results()
+        {
+            Assert.Less(Math.Abs(_outputAtopSecondAirLayer.IntSurFib - 0.051616), 0.000001);
+            Assert.Less(Math.Abs(_outputAtopSecondAirLayer.IntSurFib2 - 0.015710), 0.000001);
+            Assert.AreEqual(17, _outputAtopSecondAirLayer.IntSurFib_TallyCount);
+        }
+
+        /// <summary>
+        /// Case 3) Test to validate fiber at outer cylinder surface fully open with dot normal positive. 
         /// Validation values based on prior test.  
         /// </summary>
         [Test]
@@ -215,7 +242,7 @@ namespace Vts.Test.MonteCarlo.Detectors
         }
 
         /// <summary>
-        /// Test to validate fiber at outer cylinder surface smaller NA with dot normal positive. 
+        /// Case 3) Test to validate fiber at outer cylinder surface smaller NA with dot normal positive. 
         /// Validation values based on prior test.  
         /// </summary>
         [Test]
@@ -229,7 +256,7 @@ namespace Vts.Test.MonteCarlo.Detectors
         }
 
         /// <summary>
-        /// Test to validate fiber at inner cylinder surface fully open with dot normal negative
+        /// Case 3) Test to validate fiber at inner cylinder surface fully open with dot normal negative
         /// Validation values based on prior test.
         /// </summary>
         [Test]
@@ -242,7 +269,7 @@ namespace Vts.Test.MonteCarlo.Detectors
         }
 
         /// <summary>
-        /// Test to validate fiber at inner cylinder surface smaller NA with dot normal negative. 
+        /// Case 3) Test to validate fiber at inner cylinder surface smaller NA with dot normal negative. 
         /// Validation values based on prior test.
         /// </summary>
         [Test]
@@ -253,30 +280,6 @@ namespace Vts.Test.MonteCarlo.Detectors
             Assert.Less(Math.Abs(detector.Mean - 0.006058), 0.000001);
             Assert.Less(Math.Abs(detector.SecondMoment - 0.001836), 0.000001);
             Assert.AreEqual(2, detector.TallyCount);
-        }
-
-        /// <summary>
-        /// Test to validate fiber at 2nd tissue layer surface fully open with dot normal positive. 
-        /// Validation values based on prior test.
-        /// </summary>
-        [Test]
-        public void Validate_fully_open_fiber_atop_second_layer_below_tissue_produces_correct_results()
-        {
-            Assert.Less(Math.Abs(_outputAtopSecondTissueLayer.IntSurFib - 0.073386), 0.000001);
-            Assert.Less(Math.Abs(_outputAtopSecondTissueLayer.IntSurFib2 - 0.022474), 0.000001);
-            Assert.AreEqual(24, _outputAtopSecondTissueLayer.IntSurFib_TallyCount);
-        }
-
-        /// <summary>
-        /// Test to validate fiber at 2nd air layer surface fully open with dot normal positive. 
-        /// Validation values based on prior test.
-        /// </summary>
-        [Test]
-        public void Validate_fully_open_fiber_atop_second_layer_below_air_produces_correct_results()
-        {
-            Assert.Less(Math.Abs(_outputAtopSecondAirLayer.IntSurFib - 0.051616), 0.000001);
-            Assert.Less(Math.Abs(_outputAtopSecondAirLayer.IntSurFib2 - 0.015710), 0.000001);
-            Assert.AreEqual(17, _outputAtopSecondAirLayer.IntSurFib_TallyCount);
         }
     }
 }
